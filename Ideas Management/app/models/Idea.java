231f7eb062861b5b7239dc70d6ba5197a8a5dfab
elhadiahmed3 <elhadiahmed3@gmail.com>
2011-04-24 15:59:03 +0000

    C3S10 Created the Class Idea with most of the relations commented till the other classes are ready to use

---
+++ Ideas Management/app/models/Idea.java
@@ -0,0 +1,45 @@
+package models;
+
+import java.util.ArrayList;
+
+import javax.persistence.*;
+
+import play.data.validation.*;
+import play.db.jpa.Model;
+
+/**
+ * @author ${Ahmed El-Hadi}
+ * 
+ */
+
+public class Idea extends Model {
+	@Required
+	String title;
+	@Required
+	String description;
+	int spamCounter;
+	@Required
+	int privacyLevel;
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	// ArrayList<Tag> tagsList;
+	@OneToMany(cascade = CascadeType.PERSIST)
+	// ArrayList<Comment> commentsList;
+	@Required
+	@ManyToOne
+	// Topic belongsTo;
+	Boolean Active;
+
+	// @Required
+	// @ManyToOne
+	// public User author;
+
+	public Idea(String t, String d) {
+		this.title = t;
+		this.description = d;
+		this.Active = false;
+		// this.tagsList = new ArrayList<Tag>();
+		// this.commentsList = new ArrayList<Comment>();
+
+	}
+
+}
