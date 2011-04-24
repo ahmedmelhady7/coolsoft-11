9dbfe5ef92439bb61bcf931379c79bf7fc3b5c9a
aliaelbolock <aliaelbolock@gmail.com>
2011-04-24 16:43:19 +0000

    C3S1 description: Topic class with all its attributes, constructor and the relationship with the user

--- Ideas Management/app/models/User.java
+++ Ideas Management/app/models/User.java
@@ -1,46 +1,17 @@
 package models;
 
-import java.util.Date;
+import java.util.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
-import play.db.jpa.Model;
+public class User {
+	@OneToMany(mappedBy="creator", cascade=CascadeType.ALL)
+	public ArrayList<Topic> topicsCreated;
+	
+	@ManyToMany(mappedBy="organizers", cascade=CascadeType.ALL)
+	public ArrayList<Topic> topicsIOrganize;
 
-@Entity
-public class User extends Model {
-	public String email;
-	public String password;
-	public String firstName;
-	public String lastName;
-	public String username;
-	public String country;
-	public Date dateofBirth;
-	public int communityContributionCounter;
 
-	// Arraylist<roles> roles;
-	// Arraylist<NP> nofications;
-	// Arraylist<Request> requests;
-	// Arraylist<Comment> commentsPosted
-	// Arraylist<Notifications> notifications ;
-	// Arraylist<LinkDuplicates> linkDuplicates;
-	// Arraylist<AssignRequests> assignRequests;
-	// Arraylist<volunteerRequests> volunteerRequests;
-	// Arraylist<Invitation> invitations;
-	// Arraylist<RequestToJoin> requestsToJoin;
-	// Arraylist<RequestOfRelationship> requestRelationship;
-	// Arraylist<TopicInvitation> topicInvitations;
-
-	public User(String email, String password, String firstName,
-			String lastName, String username, int communityContributionCounter,
-			Date dateofBirth, String country) {
-		this.email = email;
-		this.username = username;
-		this.password = password;
-		this.firstName = firstName;
-		this.communityContributionCounter = communityContributionCounter;
-		this.dateofBirth = dateofBirth;
-		this.country = country;
-		this.lastName = lastName;
-
-	}
+	
+	
 }
