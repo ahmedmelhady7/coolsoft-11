1b6dcfbef3bffd50ab651e9c5cf8474499149426
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-04-24 21:25:47 +0000

    C1S7 changed the attribute "role" on Role Class to "roleName"

--- Ideas Management/app/models/Role.java
+++ Ideas Management/app/models/Role.java
@@ -8,13 +8,13 @@ import play.db.jpa.*;
  
 @Entity
 public class Role extends Model {
- String role;
+ String roleName;
  
  @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
  ArrayList  <Action> actions;
 
 public Role (String role , ArrayList<Action> actions){
-	this.role = role;
+	this.roleName = role;
 	this.actions = actions; 
 }
 
