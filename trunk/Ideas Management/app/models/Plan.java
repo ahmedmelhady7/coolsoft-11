114eae009667592fe674b388cda02d55b98191b0
salma.qayed@gmail.com <salma.qayed@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-24 23:35:49 +0000

    C5S1: added topic attribute in the constructor of the Plan class

--- trunk/Ideas Management/app/models/Plan.java
+++ trunk/Ideas Management/app/models/Plan.java
@@ -9,30 +9,34 @@ import play.db.jpa.*;
 public class Plan extends Model {
 
 	public String title;
-	// public User user;
+	public User user;
 	public int rate;
 	public String status;
 	public float progress;
 	public Date startDate;
 	public ArrayList<String> requirements;
 	public Date endDate;
-	// public Topic topic;
+	public Topic topic;
 
-	// @OneToMany(mappedBy="plan", cascade=CascadeType.ALL)
-	// public List<Tag> tags;
+	@OneToMany(mappedBy = "plan", cascade = CascadeType.ALL)
+	public List<Tag> tags;
+	
 	// @OneToMany(mappedBy="plan", cascade=CascadeType.ALL)
 	// public List<Comment> comments;
-	// @OneToMany(mappedBy="plan", cascade=CascadeType.ALL)
-	// public List<Idea> ideas;
+	
+	@OneToMany(mappedBy = "plan", cascade = CascadeType.ALL)
+	public List<Idea> ideas;
+	
 	@Lob
 	public String description;
 
 	@OneToMany(mappedBy = "plan", cascade = CascadeType.ALL)
 	public ArrayList<Item> items;
 
-	public Plan(String title, int rate, String status, Date startDate,
-			Date endDate, String description) {
+	public Plan(String title, User user, int rate, String status, Date startDate,
+			Date endDate, String description, Topic topic) {
 		this.title = title;
+		this.user = user;
 		this.rate = rate;
 		this.status = "new";
 		this.progress = 0;
@@ -41,10 +45,10 @@ public class Plan extends Model {
 		this.description = description;
 		this.requirements = new ArrayList<String>();
 		this.items = new ArrayList<Item>();
-		// still need to add topic and user
-		// this.tags = new ArrayList<Tag>();
-		// this.comments = new ArrayList<Comment>();
-		// this.ideas = new ArrayList<Idea>();
+		this.tags = new ArrayList<Tag>();
+		//this.comments = new ArrayList<Comment>();
+		this.ideas = new ArrayList<Idea>();
+		this.topic = topic;
 
 	}
 
