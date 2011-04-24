33c227c04ce3abbe00527f3377d0e5e603dcc343
omarmafaruki@gmail.com <omarmafaruki@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-24 19:16:18 +0000

    C2S1 Model Class of Organization(Incomplete), still need Invitation and Logs classes and type of privacy level

---
+++ trunk/Ideas Management/app/models/Organization.java
@@ -0,0 +1,43 @@
+package models;
+
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+@Entity
+public class Organization {
+	String name;
+	@OneToMany(mappedBy = "name")
+	public ArrayList<MainEntity> entitiesList;
+	@ManyToOne
+	public User creator;
+	@ManyToMany
+	public ArrayList<User> followers;
+	@ManyToMany
+	public ArrayList<User> enrolledUsers;
+	@ManyToMany
+	public ArrayList<Tag> relatedTags;
+	
+	//privacy level??
+	
+	
+	//Request_to_Join??
+	
+	@OneToMany//(mappedBy = Pending class creation) 
+	public ArrayList<Invitation> invitations;
+	@OneToMany//(mappedBy = Pending class creation)
+	public ArrayList<Log> logsList;
+	
+	public Organization(String name, User creator) {
+		this.name = name;
+		this.creator = creator;
+		//default privacy level
+	}
+	public Organization(String name, User creator, String privacyLevel) {
+		this.name = name;
+		this.creator = creator;
+		//default privacy level
+	}
+}
