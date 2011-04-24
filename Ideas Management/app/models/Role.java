4395bc8433eb6569183f5e1b4559a8288e7c39dd
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-04-24 21:17:04 +0000

    C1S7 change the (mapped by) part to role

--- Ideas Management/app/models/Role.java
+++ Ideas Management/app/models/Role.java
@@ -10,7 +10,7 @@ import play.db.jpa.*;
 public class Role extends Model {
  String role;
  
- @OneToMany(mappedBy = "actions", cascade = CascadeType.ALL)
+ @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
  ArrayList  <Action> actions;
 
 public Role (String role , ArrayList<Action> actions){
