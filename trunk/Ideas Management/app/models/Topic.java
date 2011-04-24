7cd95644519977b1e5f71e37601b5a3df4711c77
aliaelbolock@gmail.com <aliaelbolock@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-24 16:43:19 +0000

    C3S1 description: Topic class with all its attributes, constructor and the relationship with the user

---
+++ trunk/Ideas Management/app/models/Topic.java
@@ -0,0 +1,49 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.jpa.*;
+
+@Entity
+public class Topic extends Model {
+	public String name;
+	public String description;
+	public short privacyLevel;// I don't know the type of this attribute
+	//public ArrayList<Tag> tags;
+	//public ArrayList<Relationship> relationships;
+
+	@ManyToOne
+	public User creator;// add between topic and user (post) exactly one user
+						// can post many topics
+
+	@ManyToMany
+	public ArrayList<User> organizers;// topic belongs to which entity and get
+										// the list of its organizers
+	public ArrayList<Idea> ideas;
+	//public ArrayList<Comment> commentsOn;
+	//public ArrayList<RequestToJoin> requestsToJoin;
+	//public ArrayList<RequestOfRelationship> relationshipRequests;
+	//public ArrayList<TopicInvitation> topicInvitations;
+	//public ArrayList<OrganizationEntity> entities;// change name of class Entity
+													// as it leads to conflicts
+													// with the Entity in java
+													// in persistence
+													// add this attribute
+	public ArrayList<Plan> plans;// add this attribute
+
+	public Topic(String n, String d, short p, User c) {
+		name = n;
+		description = d;
+		privacyLevel = p;
+		creator = c;
+		//tags = new ArrayList<Tag>();
+		//relationships = new ArrayList<Relationship>();
+		organizers = new ArrayList<User>();
+		ideas = new ArrayList<Idea>();
+		//commentsOn = new ArrayList<Comment>();
+		//requestsToJoin = new ArrayList<RequestToJoin>();
+	}
+	
+
+}
