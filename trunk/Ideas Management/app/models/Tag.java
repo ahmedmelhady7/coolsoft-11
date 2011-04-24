d2c40fb39a6311b92dbbdc6ba09792d06a23ba35
omarmafaruki@gmail.com <omarmafaruki@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-24 19:15:41 +0000

    Minor changes to class, added db relations

--- trunk/Ideas Management/app/models/Tag.java
+++ trunk/Ideas Management/app/models/Tag.java
@@ -3,19 +3,26 @@ package models;
 import java.util.ArrayList;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 
+import play.data.validation.Required;
 import play.db.jpa.Model;
 @Entity
 public class Tag extends Model{
+	@Required
 	String name;
-	ArrayList<Tag> related_tags;
+	ArrayList<Tag> relatedTags;
 	//String creator;	We might still need this attribute;
+	@ManyToMany
+	public ArrayList<User> followers;
+	@ManyToMany
+	public ArrayList<Organization> organizations;
 	
 	public Tag(String name) {
 		this.name = name;
 	}
 	public Tag(String name, ArrayList<Tag> tags) {
 		this.name = name;
-		this.related_tags = tags;
+		this.relatedTags = tags;
 	}
 }
