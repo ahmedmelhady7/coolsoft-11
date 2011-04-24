e46ea8c6806d62a5337e7ef56ead008d2b5231e3
mostafa.aly0 <mostafa.aly0@gmail.com>
2011-04-24 17:32:50 +0000

    C1S9 re-committing the User model after updating it , updating the instance variables and the constructor

--- Ideas Management/app/models/User.java
+++ Ideas Management/app/models/User.java
@@ -1,18 +1,74 @@
 package models;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
-public class User {
-	@OneToMany(mappedBy="creator", cascade=CascadeType.ALL)
+import play.db.jpa.Model;
+
+@Entity
+public class User extends Model {
+	public String email;
+	public String password;
+	public String firstName;
+	public String lastName;
+	public String username;
+	public String country;
+	public Date dateofBirth;
+	public int communityContributionCounter;
+
+	ArrayList<Role> roles;
+	@OneToMany(mappedBy = "creator", cascade = CascadeType.ALL)
 	public ArrayList<Topic> topicsCreated;
-	
-	@ManyToMany(mappedBy="organizers", cascade=CascadeType.ALL)
+	@ManyToMany(mappedBy = "organizers", cascade = CascadeType.ALL)
 	public ArrayList<Topic> topicsIOrganize;
-
-
-	@OneToMany(mappedBy="creator", cascade=CascadeType.ALL)
+	@OneToMany(mappedBy = "creator", cascade = CascadeType.ALL)
 	public ArrayList<Idea> ideasCreated;
-	
+
+	// ArrayList<AssignRequest> assignRequests;
+	// @OneToMany (mappedBy = user)
+	// ArrayList<NotificationProfile> noficationProfiles;
+	// @OneToMany (mappedBy = directedTo)
+	// ArrayList<Notification> notifications ;
+	// ArrayList<Request> requests;
+	// ArrayList<Comment> commentsPosted;
+	// ArrayList<LinkDuplicates> linkDuplicates;
+	// ArrayList<volunteerRequests> volunteerRequests;
+	// ArrayList<Invitation> invitations;
+	// ArrayList<RequestToJoin> requestsToJoin;
+	// ArrayList<RequestOfRelationship> requestRelationship;
+	// ArrayList<TopicInvitation> topicInvitations;
+
+	public User(String email, String password, String firstName,
+			String lastName, String username, int communityContributionCounter,
+			Date dateofBirth, String country) {
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.communityContributionCounter = communityContributionCounter;
+		this.dateofBirth = dateofBirth;
+		this.country = country;
+		this.lastName = lastName;
+		roles = new ArrayList<Role>();
+		ideasCreated = new ArrayList<Idea>();
+		topicsIOrganize = new ArrayList<Topic>();
+		topicsCreated = new ArrayList<Topic>();
+		// notification= new ArrayList<NotificationProfile>();
+		// requests=new ArrayList<Request>();
+		// commentsPosted = new ArrayList<Comment>();
+		// linkDuplicates = new ArrayList<LinkDuplicates>();
+		// assignRequests = new ArrayList<AssignRequests>();
+		// volunteerRequests = new ArrayList<volunteerRequests>();
+		// invitations = new ArrayList<Invitation>();
+		// requestsToJoin = new ArrayList<RequestToJoin>();
+		// requestRelationship = new ArrayList<RequestOfRelationship>();
+		// topicInvitations = new ArrayList<TopicInvitation>() ;
+
+	}
+
 }
