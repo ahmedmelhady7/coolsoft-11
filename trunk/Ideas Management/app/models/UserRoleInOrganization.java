d0a32b4c7e51b18596a4b541f4939d616e6192a3
nada.ossama.kamal@gmail.com <nada.ossama.kamal@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-24 23:33:53 +0000

    C1S7 Creating a Model that will map the role of a  user in a certain entity and whether he is blocked or not , the role of a user might be Organization Lead , entity Organizer, Topic Organizer or Idea Developer

--- trunk/Ideas Management/app/models/UserRoleInOrganization.java
+++ trunk/Ideas Management/app/models/UserRoleInOrganization.java
@@ -1,5 +1,40 @@
 package models;
 
-public class UserRoleInOrganization {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import play.db.jpa.Model;
+
+@Entity
+public class UserRoleInOrganization extends Model {
+
+	@OneToMany
+	List<User> enrolled;
+
+	@OneToMany
+	List<Organization> organization;
+
+	@OneToMany
+	List<Role> roles;
+	// this represents the ID of the entity or the topic that an organizer
+	// manages
+	int entityTopicID;
+
+	boolean blocked;
+
+	public UserRoleInOrganization(List<User> enrolled,
+			List<Organization> organization, List<Role> role, int eTID,
+			boolean blocked) {
+		this.enrolled = enrolled;
+		this.organization = organization;
+		this.roles = role;
+		entityTopicID = eTID;
+		this.blocked = blocked;
+
+	}
+	
 
 }
