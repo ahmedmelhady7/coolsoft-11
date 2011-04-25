910bd22034ad9d9611f686463f10aee07332a028
Mohamedmohie92 <mohamedmohie92@gmail.com>
2011-04-25 19:11:16 +0000

    C5S7: Added viewAsList method to Plans class. Created viewAsList.html view file.

--- Ideas Management/app/controllers/Plans.java
+++ Ideas Management/app/controllers/Plans.java
@@ -1,11 +1,16 @@
 package controllers;
 
-import java.util.Date;
-
 import models.Item;
 import models.Plan;
+import java.util.List;
 
 public class Plans extends CRUD {
 
+	public static void viewAsList(long id) {
+		Plan p = Plan.findById(id);
+		List<Item> itemsList = p.items;
+		render(p, itemsList);
+	}
+	
 	
 }
