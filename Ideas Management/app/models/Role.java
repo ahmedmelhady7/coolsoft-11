f5c6dc66f676d3cb2288e1f49a60378eee6a02d6
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-04-25 09:11:26 +0000

    C1S7 changed the constructor

--- Ideas Management/app/models/Role.java
+++ Ideas Management/app/models/Role.java
@@ -1,22 +1,28 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.*;
- 
+
 import play.db.jpa.*;
- 
+
 @Entity
 public class Role extends Model {
- String roleName;
- 
- @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
- Set  <Action> actions;
+	String roleName;
 
-public Role (String role , Set<Action> actions){
-	this.roleName = role;
-	this.actions = actions; 
-}
+	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+	List<Action> actions;
+
+	@OneToMany(mappedBy = "role" , cascade = CascadeType.ALL)
+	List<UserRoleInOrganization> userRoleInOrganization;
+
+	public Role(String role) {
+		this.roleName = role;
+		this.actions = new ArrayList<Action>();
+		this.userRoleInOrganization = new ArrayList<UserRoleInOrganization> ();
+	//	this.userRoleInOrganization = userRoleInOrganization;
+	}
 
 }
