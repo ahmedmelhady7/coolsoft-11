1ad8d75f7be893fb65a70846180030d29ee48dc3
salma.qayed <salma.qayed@gmail.com>
2011-04-25 15:53:13 +0000

    C5S11: created viewAssignedItems.html file and added viewAssignedItems method to Items class

--- Ideas Management/app/controllers/Items.java
+++ Ideas Management/app/controllers/Items.java
@@ -1,5 +1,15 @@
 package controllers;
 
+import java.util.List;
+
+import models.Item;
+import models.User;
+
 public class Items extends CRUD {
 
+	public static void viewAssignedItems(long userid) {
+		User user = User.findById(userid);
+		List<Item> assignedItems = Item.find("? in assignees", user).fetch();
+		render(assignedItems);
+	}
 }
--- Ideas Management/app/controllers/VolunteerRequests.java
+++ Ideas Management/app/controllers/VolunteerRequests.java
@@ -1,5 +1,15 @@
 package controllers;
 
+import models.Item;
+import models.User;
+import models.VolunteerRequest;
+
 public class VolunteerRequests extends CRUD{
 
+	
+	public static void sendVolunteerRequest(long senderid, long itemid, String justification) {
+	User sender = User.findById(senderid);
+	Item dest = Item.findById(itemid);
+	VolunteerRequest volunteerRequest = new VolunteerRequest(sender, dest, justification).save();
+	}
 }
