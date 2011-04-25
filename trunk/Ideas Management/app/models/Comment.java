e316c7dcc287d5e8ba2d7070a3d199f5a65ab21e
ibrahim.yehia.safwat@gmail.com <ibrahim.yehia.safwat@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-25 21:42:48 +0000

    C4S8 Comment or share any idea, plane, request if they are public or I have a permission

---
+++ trunk/Ideas Management/app/models/Comment.java
@@ -0,0 +1,28 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import play.db.jpa.Model;
+
+@Entity
+public class Comment extends Model {
+	
+	String comment;
+	
+	public int ID;
+		
+	@OneToMany
+	String postedOn;
+	
+	@OneToMany
+	User commenter;
+	
+	public Comment(String comment, int ID, String postedOn, User commenter)
+	{
+		this.comment=comment;
+		this.ID=ID;
+		this.postedOn=postedOn;
+		this.commenter=commenter;
+	}
+
+}
