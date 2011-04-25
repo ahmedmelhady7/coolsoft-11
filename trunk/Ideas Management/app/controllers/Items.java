fbcc0587cb7be22ff515284247cc98bc214f1c67
salma.qayed@gmail.com <salma.qayed@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-25 16:35:27 +0000

    C5S11: modified method viewAssignedItems method in Items class

--- trunk/Ideas Management/app/controllers/Items.java
+++ trunk/Ideas Management/app/controllers/Items.java
@@ -9,7 +9,6 @@ public class Items extends CRUD {
 
 	public static void viewAssignedItems(long userid) {
 		User user = User.findById(userid);
-		List<Item> assignedItems = Item.find("? in assignees", user).fetch();
-		render(assignedItems);
+		render(user.itemsAssigned);
 	}
 }
