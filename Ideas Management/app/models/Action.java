e295cb44e3d7252650ad5cb7a8e98c8ac9250e82
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-04-25 14:42:15 +0000

    C1S7 added the method equals to the class action as to compare two actions

--- Ideas Management/app/models/Action.java
+++ Ideas Management/app/models/Action.java
@@ -11,7 +11,7 @@ import play.db.jpa.*;
 public class Action extends Model {
 	@Lob
 	String description;
-	
+
 	@ManyToOne
 	Role role;
 
@@ -22,6 +22,21 @@ public class Action extends Model {
 		this.description = description;
 		this.role = role;
 		bannedUsers = new ArrayList<BannedUser>();
+	}
+
+	// equals takes an action and returns whether this action equals to the
+	// action the method is invoked on
+	// @Parm a Action is the action given
+	// returns boolean true if the passed action equals the invoked one 
+	// or false if they are not equal
+	
+	public boolean equals(Action a) {
+		if (this.description.equals(a.description)) {
+			return true;
+		}
+
+		return false;
+
 	}
 
 }
