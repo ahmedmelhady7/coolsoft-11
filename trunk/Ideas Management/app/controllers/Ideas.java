c7444ea2351a4ef8f217c76b2a7bead92bdb40b1
abdalrahman.mursi@gmail.com <abdalrahman.mursi@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-25 18:37:36 +0000

    C3S13 a method that saves an idea as a draft is created and documented

--- trunk/Ideas Management/app/controllers/Ideas.java
+++ trunk/Ideas Management/app/controllers/Ideas.java
@@ -3,10 +3,39 @@
  */
 package controllers;
 
+import java.util.ArrayList;
+
+import models.Idea;
+import models.Tag;
+import models.Topic;
+import models.User;
+
 /**
  * @author ${Ahmed El-Hadi}
  * 
  */
 public class Ideas extends CRUD {
-
+	
+	/*
+	 * @author Abdalrahman Ali
+	 * 
+	 * this method saves an idea as a draft
+	 * 
+	 * @param title
+	 * 		the title of the idea
+	 * @param body
+	 * 		the body of the idea
+	 * @param topic
+	 * 		the topic that the idea belongs to
+	 * @param user
+	 * 		the user saving this idea
+	 * 
+	 * @return void
+	 * 
+	 * */
+	
+	public static void saveDraft(String title,String body,Topic topic,User user )
+	{
+		Idea idea = new Idea(title,body,user,topic,true);
+	}
 }
