23b0097140710675710413e07926f5d37f46c1cf
nada.ossama.kamal@gmail.com <nada.ossama.kamal@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-25 08:57:31 +0000

    C1S7 added @Lob to action description and the banned user

--- trunk/Ideas Management/app/models/Action.java
+++ trunk/Ideas Management/app/models/Action.java
@@ -1,23 +1,27 @@
 package models;
 
-
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.*;
- 
+
 import play.db.jpa.*;
- 
+
 @Entity
 public class Action extends Model {
+	@Lob
 	String description;
-    @ManyToOne
-     Role role;
-    
-public Action (String description,Role role){
-	this.description = description;
-	this.role = role;
-}
+	
+	@ManyToOne
+	Role role;
+
+	@OneToMany
+	List<BannedUser> bannedUsers;
+
+	public Action(String description, Role role) {
+		this.description = description;
+		this.role = role;
+		bannedUsers = new ArrayList<BannedUser>();
+	}
 
 }
-
-
