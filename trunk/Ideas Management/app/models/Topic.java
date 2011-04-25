baeabcf314165efbe8742cd631f57edfd8f687d7
aliaelbolock@gmail.com <aliaelbolock@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-25 17:56:29 +0000

    C3S1 description: Changed ArrayList<> to List<> in Topic and added another constructor of Topic with MainEntity as input.

--- trunk/Ideas Management/app/models/Topic.java
+++ trunk/Ideas Management/app/models/Topic.java
@@ -1,8 +1,8 @@
 package models;
 
+
 import java.nio.MappedByteBuffer;
 import java.util.*;
-
 import javax.persistence.*;
 
 import play.db.jpa.*;
@@ -11,64 +11,61 @@ import play.db.jpa.*;
 public class Topic extends Model {
 	public String name;
 	public String description;
-	public short privacyLevel;// I don't know the type of this attribute
-	public ArrayList<Tag> tags;
-	// public ArrayList<Relationship> relationships;
+	public short privacyLevel;
+	public List<Tag> tags;
+	//public List<Relationship> relationships;
 
 	@ManyToOne
-	public User creator;// add between topic and user (post) exactly one user
-						// can post many topics
+	public User creator;
 
 	@ManyToMany
-	public ArrayList<User> organizers;// topic belongs to which entity and get
-	// the list of its organizers
+	public List<User> organizers;
+	public List<User> followers;
 	@OneToMany(mappedBy = "belongsToTopic", cascade = CascadeType.ALL)
-	public ArrayList<Idea> ideas;
-
+	public List<Idea> ideas;
+	//public List<Comment> commentsOn;
+	//public List<RequestToJoin> requestsToJoin;
+	//public List<RequestOfRelationship> relationshipRequests;
+	//public List<TopicInvitation> topicInvitations;
 	@ManyToOne
 	public MainEntity entity;
-	// public ArrayList<Comment> commentsOn;
-	// public ArrayList<RequestToJoin> requestsToJoin;
-	// public ArrayList<RequestOfRelationship> relationshipRequests;
-	// public ArrayList<TopicInvitation> topicInvitations;
-	// public ArrayList<OrganizationEntity> entities;// change name of class
-	// Entity
-	// as it leads to conflicts
-	// with the Entity in java
-	// in persistence
-	// add this attribute
-	public ArrayList<Plan> plans;// add this attribute
+	public List<Plan> plans;
 
 	@OneToMany(mappedBy = "topic", cascade = CascadeType.ALL)
 	 List<Invitation> invitation;
 	
+	
+	public Topic(String n, String d, short p, User c, MainEntity m) {
+		name = n;
+		description = d;
+		privacyLevel = p;
+		creator = c;
+		entity = m;
+		//tags = new ArrayList<Tag>();
+		//relationships = new ArrayList<Relationship>();
+		organizers = new ArrayList<User>();
+		ideas = new ArrayList<Idea>();
+		invitation = new ArrayList<Invitation>();
+		//commentsOn = new ArrayList<Comment>();
+		//requestsToJoin = new ArrayList<RequestToJoin>();
+	}
 	
 	public Topic(String n, String d, short p, User c) {
 		name = n;
 		description = d;
 		privacyLevel = p;
 		creator = c;
-		// tags = new ArrayList<Tag>();
-		// relationships = new ArrayList<Relationship>();
+		//tags = new ArrayList<Tag>();
+		//relationships = new ArrayList<Relationship>();
 		organizers = new ArrayList<User>();
 		ideas = new ArrayList<Idea>();
 		invitation = new ArrayList<Invitation>();
-		// commentsOn = new ArrayList<Comment>();
-		// requestsToJoin = new ArrayList<RequestToJoin>();
+		//commentsOn = new ArrayList<Comment>();
+		//requestsToJoin = new ArrayList<RequestToJoin>();
 	}
-
-	/**
-	 * This Method returns a list of organizers in a certain topic
-	 * 
-	 * @author 	lama.ashraf
-	 * 
-	 * @story 	C1S12
-	 * 
-	 * @return	ArrayList<User>
-	 */
-	public ArrayList<User> getOrganizer() {
-		return organizers;
-
+	 
+	public List<User> getOrganizer(){
+		 return organizers;
 	}
 	
 	/**
@@ -87,5 +84,5 @@ public class Topic extends Model {
 	public void unfollow(User user) {
 		followers.remove(user);
 	}
-*/
+	*/
 }
