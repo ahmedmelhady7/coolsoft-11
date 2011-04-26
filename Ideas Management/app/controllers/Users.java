e964af10d9872b8e62e22104a0870f69ee329a59
elhadiahmed3 <elhadiahmed3@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-26 20:31:00 +0000

    fixed some modifiers in the class Idea and added a method in class Users for reporting an idea as a spam but it's not yet completed

--- Ideas Management/app/controllers/Users.java
+++ Ideas Management/app/controllers/Users.java
@@ -1,13 +1,8 @@
 package controllers;
 
-import models.MainEntity;
-import models.Organization;
-import models.Tag;
-import models.Topic;
-import models.User;
+import models.*;
 
-public class Users extends CRUD
-{
+public class Users extends CRUD {
 	/**
 	 * This Method removes a user from the list of followers in a given Topic
 	 * 
@@ -27,59 +22,71 @@ public class Users extends CRUD
 		topic.unfollow(user);
 		user.unfollow(topic);
 	}
+
 	/**
 	 * This Method removes a user from the list of followers in a given Tag
 	 * 
-	 * @author 	Ibrahim.al.khayat
+	 * @author Ibrahim.al.khayat
 	 * 
-	 * @story 	C2S12
+	 * @story C2S12
 	 * 
-	 * @param 	tag 	: the tag that the user is following
+	 * @param tag
+	 *            : the tag that the user is following
 	 * 
-	 * @param  	user 	: the user who follows
+	 * @param user
+	 *            : the user who follows
 	 * 
-	 * @return	void
+	 * @return void
 	 */
 	public static void unfollow(Tag tag, User user) {
 		tag.unfollow(user);
 		user.unfollow(tag);
 	}
-	
+
 	/**
 	 * This Method removes a user from the list of followers in an organization
 	 * 
-	 * @author 	Ibrahim.al.khayat
+	 * @author Ibrahim.al.khayat
 	 * 
-	 * @story 	C2S12
+	 * @story C2S12
 	 * 
-	 * @param   org     : the organization the user is following
+	 * @param org
+	 *            : the organization the user is following
 	 * 
-	 * @param  	user 	: the user who follows
+	 * @param user
+	 *            : the user who follows
 	 * 
-	 * @return	void
+	 * @return void
 	 */
-	
+
 	public static void unfollow(Organization org, User user) {
 		org.unfollow(user);
 		user.unfollow(org);
 	}
-	
+
 	/**
 	 * This Method removes a user from the list of followers in an entity
 	 * 
-	 * @author 	Ibrahim.al.khayat
+	 * @author Ibrahim.al.khayat
 	 * 
-	 * @story 	C2S12
+	 * @story C2S12
 	 * 
-	 * @param   entity  : the entity the user is following
+	 * @param entity
+	 *            : the entity the user is following
 	 * 
-	 * @param  	user 	: the user who follows
+	 * @param user
+	 *            : the user who follows
 	 * 
-	 * @return	void
+	 * @return void
 	 */
-	
+
 	public static void unfollow(MainEntity entity, User user) {
 		entity.unfollow(user);
 		user.unfollow(entity);
+	}
+
+	public static void reportIdeaAsSpam(Idea idea, Organization organization,
+			User reporter) {
+		idea.spamCounter++;
 	}
 }
