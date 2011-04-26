3702e49a6f7c041664c7dabb3f0134ff6642f1cd
aliaelbolock@gmail.com <aliaelbolock@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-26 21:09:55 +0000

    C3S20 description: added method getIdeas Added the first part of the relation between topic and comment

--- trunk/Ideas Management/app/models/Topic.java
+++ trunk/Ideas Management/app/models/Topic.java
@@ -3,6 +3,7 @@ package models;
 
 import java.nio.MappedByteBuffer;
 import java.util.*;
+
 import javax.persistence.*;
 import play.data.validation.*;
 
@@ -10,64 +11,155 @@ import play.db.jpa.*;
 
 @Entity
 public class Topic extends Model {
+	
+
+	/**
+	 * the name of the topic
+	 */
 	@Required
 	public String name;
+	
+	/**
+	 * the description of the topic
+	 */
 	@Lob
 	public String description;
+	
+	/**
+	 * the privacy level of the topic, restricts who can do what with it
+	 */
 	public short privacyLevel;
 	
+	/**
+	 * the list of tags the topic is tagged with
+	 */
 	@ManyToMany
 	public List<Tag> tags;
+	
+	/**
+	 * the list of relationships the topic has
+	 */
 	//public List<Relationship> relationships;
-
+	
+	/**
+	 * the creator of the topic
+	 */
 	@ManyToOne
 	public User creator;
-
+	
+	/**
+	 * the list of topic organizers
+	 */
 	@ManyToMany
 	public List<User> organizers;
+	
+	/**
+	 * the list of followers of the topic
+	 */
 	@ManyToMany
 	public List<User> followers;
+	
+	/**
+	 * the list of ideas in the topic
+	 */
 	@OneToMany(mappedBy = "belongsToTopic", cascade = CascadeType.ALL)
 	public List<Idea> ideas;
-	//public List<Comment> commentsOn;
+	
+	/**
+	 * the list of comments on the topic
+	 */
+	@OneToMany
+	public List<Comment> commentsOn;
+	
+	/**
+	 * the list of requests to join the topic
+	 */
 	//public List<RequestToJoin> requestsToJoin;
+	
+	/**
+	 * the list of relationship requests with the topic
+	 */
 	//public List<RequestOfRelationship> relationshipRequests;
+	
+	/**
+	 * the list of invitations to the topic?
+	 */
 	//public List<TopicInvitation> topicInvitations;
+	
+	/**
+	 * the entity the topic is in
+	 */
 	@ManyToOne
 	public MainEntity entity;
+	
+	/**
+	 * the plan that the topic is promoted to
+	 */
 	@OneToOne
 	public Plan plan;
-
+	
+	/**
+	 * the list of invitations to the topic?
+	 */
 	@OneToMany(mappedBy = "topic", cascade = CascadeType.ALL)
 	 List<Invitation> invitation;
 	
-	
+	/**
+	 * Default constructor that creates a topic with name, description,privacy level, creator and entity
+	 * 
+	 * @author ${aliaelbolock}
+	 * 
+	 * @param name
+	 *            title of the topic
+	 * @param description
+	 *            description of the topic
+	 * @param privacyLevel           
+	 *            the privacy level of the topic
+	 * @param creator
+	 *            Author of the topic
+	 * @param entity
+	 *            Entity that the topic belongs/added to
+	 */
 	public Topic(String n, String d, short p, User c, MainEntity m) {
 		name = n;
 		description = d;
 		privacyLevel = p;
 		creator = c;
 		entity = m;
-		//tags = new ArrayList<Tag>();
+		tags = new ArrayList<Tag>();
 		//relationships = new ArrayList<Relationship>();
 		organizers = new ArrayList<User>();
 		ideas = new ArrayList<Idea>();
 		invitation = new ArrayList<Invitation>();
-		//commentsOn = new ArrayList<Comment>();
+		commentsOn = new ArrayList<Comment>();
 		//requestsToJoin = new ArrayList<RequestToJoin>();
 	}
 	
+	/**
+	 * Creates a topic with name, description,privacy level and creator
+	 * 
+	 * @author ${aliaelbolock}
+	 * 
+	 * @param name
+	 *            title of the topic
+	 * @param description
+	 *            description of the topic
+	 * @param privacyLevel           
+	 *            the privacy level of the topic
+	 * @param creator
+	 *            Author of the topic
+	 */
 	public Topic(String n, String d, short p, User c) {
 		name = n;
 		description = d;
 		privacyLevel = p;
 		creator = c;
-		//tags = new ArrayList<Tag>();
+		tags = new ArrayList<Tag>();
 		//relationships = new ArrayList<Relationship>();
 		organizers = new ArrayList<User>();
 		ideas = new ArrayList<Idea>();
 		invitation = new ArrayList<Invitation>();
-		//commentsOn = new ArrayList<Comment>();
+		commentsOn = new ArrayList<Comment>();
 		//requestsToJoin = new ArrayList<RequestToJoin>();
 	}
 	 
@@ -82,6 +174,19 @@ public class Topic extends Model {
 	 */
 	public List<User> getOrganizer(){
 		 return organizers;
+	}
+	
+	/**
+	 * This Method returns the list of ideas in a certain topic
+	 *
+	 * @author 	aliaelbolock
+	 *
+	 * @story 	C3S20
+	 *
+	 * @return	ArrayList<Idea>
+	 */
+	public ArrayList<Idea> getIdeas(){
+		 return (ArrayList<Idea>) ideas;
 	}
 	
 	/**
