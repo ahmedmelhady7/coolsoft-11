af5ae032a422453ef0a60c789ceb287d8045e664
Mohamedmohie92 <mohamedmohie92@gmail.com>
2011-04-26 22:10:14 +0000

    C5S13: Implemented methods startItem and toggleItem in Items controller class. Edited viewAssignedItems.html to enable starting tasks and marking them as done or not done.

--- Ideas Management/app/controllers/Items.java
+++ Ideas Management/app/controllers/Items.java
@@ -14,4 +14,25 @@ public class Items extends CRUD {
 		List<Item> assignedItems = user.itemsAssigned;
 		render(assignedItems);
 	}
+
+	public static void startItem(long id) {
+		Item item = Item.findById(id);
+		if (item.status == 0) {
+			item.status = 1;
+		}
+	}
+
+	public static void toggleItem(long id) {
+		Item item = Item.findById(id);
+		switch (item.status) {
+		case 1:
+			item.status = 2;
+			break;
+		case 2:
+			item.status = 1;
+			break;
+		default:
+			break;
+		}
+	}
 }
