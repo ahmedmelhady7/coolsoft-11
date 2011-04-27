b9ec409388fc3e4e13c445f081cff40ca1c968d0
omarmafaruki@gmail.com <omarmafaruki@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-27 22:38:12 +0000

    Fixed Errors

--- trunk/Ideas Management/app/models/RequestToJoin.java
+++ trunk/Ideas Management/app/models/RequestToJoin.java
@@ -1,18 +1,27 @@
 package models;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
 import play.db.jpa.Model;
 
+@Entity
 public class RequestToJoin extends Model{
 	@ManyToOne
 	public User source;
+	
 	@ManyToOne
 	public Topic topic;
+	
 	@ManyToOne
 	public Organization organization;
 	
-	public RequestToJoin() {
-		
+	public String description;
+	
+	public RequestToJoin(User source, Topic topic, Organization organization, String des) {
+		this.source = source;
+		this.topic = topic;
+		this.organization = organization;
+		this.description = des;
 	}
 }
