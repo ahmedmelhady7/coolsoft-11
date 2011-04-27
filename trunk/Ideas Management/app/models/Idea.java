ca5632301ee33f2d8986f51a7abe97dd6c9f5f58
elhadiahmed3 <elhadiahmed3@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-27 21:16:43 +0000

    added an anotattion in the idea class @Lob and made the idea's privacy level same as topic privacy level in topics class

--- trunk/Ideas Management/app/models/Idea.java
+++ trunk/Ideas Management/app/models/Idea.java
@@ -27,6 +27,7 @@ public class Idea extends Model {
 	 * Idea's description Required for creating an idea
 	 */
 	@Required
+	@Lob
 	public String description;
 	/**
 	 * Counter for how many times the idea got reported as a spam
@@ -67,7 +68,7 @@ public class Idea extends Model {
 	 * true if the idea is not to be posted (saved as a draft) , false if it's
 	 * to be posted normally
 	 */
-	
+
 	public boolean isDraft;
 
 	public int rating;
@@ -98,7 +99,7 @@ public class Idea extends Model {
 		this.belongsToTopic = topic;
 		this.isDraft = false;
 		this.tagsList = new ArrayList<Tag>();
-		
+
 		// this.commentsList = new ArrayList<Comment>();
 
 	}
