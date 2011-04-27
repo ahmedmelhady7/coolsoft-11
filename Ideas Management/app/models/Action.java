5ebb8ba8d6c6d5a2b6841aea4b1466ebd572e5c4
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-04-27 05:46:16 +0000

    C1S7 tending to remove the whole class annd to replace it in the related classes with strings that represents the action

--- Ideas Management/app/models/Action.java
+++ Ideas Management/app/models/Action.java
@@ -1,44 +1,44 @@
-package models;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.*;
-
-import play.db.jpa.*;
-
-@Entity
-public class Action extends Model {
-	@Lob
-	String description;
-
-	@ManyToOne
-	Role role;
-
-	@OneToMany
-	List<BannedUser> bannedUsers;
-	
-	
-
-	public Action(String description, Role role) {
-		this.description = description;
-		this.role = role;
-		bannedUsers = new ArrayList<BannedUser>();
-	}
-
-	// equals takes an action and returns whether this action equals to the
-	// action the method is invoked on
-	// @Parm a Action is the action given
-	// returns boolean true if the passed action equals the invoked one 
-	// or false if they are not equal
-	
-	public boolean equals(Action a) {
-		if (this.description.equals(a.description)) {
-			return true;
-		}
-
-		return false;
-
-	}
-
-}
+//package models;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import javax.persistence.*;
+//
+//import play.db.jpa.*;
+//
+//@Entity
+//public class Action extends Model {
+//	@Lob
+//	String description;
+//
+//	@ManyToOne
+//	Role role;
+//
+//	@OneToMany
+//	List<BannedUser> bannedUsers;
+//	
+//	
+//
+//	public Action(String description, Role role) {
+//		this.description = description;
+//		this.role = role;
+//		bannedUsers = new ArrayList<BannedUser>();
+//	}
+//
+//	// equals takes an action and returns whether this action equals to the
+//	// action the method is invoked on
+//	// @Parm a Action is the action given
+//	// returns boolean true if the passed action equals the invoked one 
+//	// or false if they are not equal
+//	
+//	public boolean equals(Action a) {
+//		if (this.description.equals(a.description)) {
+//			return true;
+//		}
+//
+//		return false;
+//
+//	}
+//
+//}
