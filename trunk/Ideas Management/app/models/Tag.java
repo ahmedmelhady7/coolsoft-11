b6bbfb50f2d33ba16b7333aadc3176f7a3223af3
ibrahim.al.khayat@gmail.com <ibrahim.al.khayat@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-27 19:02:01 +0000

    C2S12 finished the story methods documentation and views

--- trunk/Ideas Management/app/models/Tag.java
+++ trunk/Ideas Management/app/models/Tag.java
@@ -9,49 +9,52 @@ import javax.persistence.ManyToMany;
 
 import play.data.validation.Required;
 import play.db.jpa.Model;
+
 @Entity
-public class Tag extends Model{
-	
+public class Tag extends Model {
+
 	/**
 	 * Tag name
 	 */
 	@Required
 	public String name;
-//	@ManyToMany(mappedBy = "relatedTags") 	Still waiting for the Relationship Class
-//	public List<Tag> relatedTags; 		  	Sprint 2
-	
-	//String creator;	We might still need this attribute;
-	
+	// @ManyToMany(mappedBy = "relatedTags") Still waiting for the Relationship
+	// Class
+	// public List<Tag> relatedTags; Sprint 2
+
+	// String creator; We might still need this attribute;
+
 	/**
 	 * List of users following the tag
 	 */
 	@ManyToMany(mappedBy = "followingTags", cascade = CascadeType.ALL)
 	public List<User> followers;
-	
+
 	/**
-	 * Added by Alia, but whoever is responsible for it please check the cascade etc.
+	 * Added by Alia, but whoever is responsible for it please check the cascade
+	 * etc.
 	 */
 	@ManyToMany(mappedBy = "tags")
 	public List<Topic> taggedTopics;
-	
+
 	/**
 	 * List of organizations tagged by the tag
 	 */
 	@ManyToMany
 	public List<Organization> organizations;
-	
+
 	/**
 	 * List of entities tagged by the tag
 	 */
 	@ManyToMany
 	public List<MainEntity> entities;
-	
+
 	/**
 	 * List of items tagged by the tag
 	 */
 	@ManyToMany
 	public List<Item> taggedItems;
-	
+
 	/**
 	 * Tag attributes
 	 * 
@@ -62,24 +65,25 @@ public class Tag extends Model{
 		this.followers = new ArrayList<User>();
 		this.organizations = new ArrayList<Organization>();
 		this.taggedItems = new ArrayList<Item>();
-		//this.relatedTags = new ArrayList<Tag>();
+		// this.relatedTags = new ArrayList<Tag>();
 	}
-	
+
 	/**
 	 * This Method removes a user from the list of followers
 	 * 
-	 * @author 	Ibrahim.al.khayat
+	 * @author Ibrahim.al.khayat
 	 * 
-	 * @story 	C2S12
+	 * @story C2S12
 	 * 
-	 * @param  	user 	: the user who follows
+	 * @param user
+	 *            : the user who follows
 	 * 
-	 * @return	void
+	 * @return void
 	 */
 	public void unfollow(User user) {
 		followers.remove(user);
+		_save();
 	}
-	
 
 	public void setName(String name) {
 		this.name = name;
--- trunk/Ideas Management/app/models/Topic.java
+++ trunk/Ideas Management/app/models/Topic.java
@@ -1,6 +1,5 @@
 package models;
 
-
 import java.nio.MappedByteBuffer;
 import java.util.*;
 
@@ -12,14 +11,13 @@ import play.db.jpa.*;
 
 @Entity
 public class Topic extends Model {
-	
 
 	/**
 	 * the name of the topic
 	 */
 	@Required
 	public String title;
-	
+
 	/**
 	 * the description of the topic
 	 */
@@ -27,95 +25,96 @@ public class Topic extends Model {
 	@Required
 	@MaxSize(10000)
 	public String description;
-	
+
 	/**
 	 * the privacy level of the topic, restricts who can do what with it
 	 */
 	public short privacyLevel;
-	
+
 	/**
 	 * the list of tags the topic is tagged with
 	 */
 	@ManyToMany
 	public List<Tag> tags;
-	
+
 	/**
 	 * the list of relationships the topic has
 	 */
-	//public List<Relationship> relationships;
-	
+	// public List<Relationship> relationships;
+
 	/**
 	 * the creator of the topic
 	 */
 	@Required
 	@ManyToOne
 	public User creator;
-	
+
 	/**
 	 * the list of topic organizers
 	 */
 	@ManyToMany
 	public List<User> organizers;
-	
+
 	/**
 	 * the list of followers of the topic
 	 */
 	@ManyToMany
 	public List<User> followers;
-	
+
 	/*
 	 * the list of users that can access the topic
 	 */
 	@ManyToMany
 	public List<User> canAccess;
-	
+
 	/**
 	 * the list of ideas in the topic
 	 */
 	@OneToMany(mappedBy = "belongsToTopic", cascade = CascadeType.ALL)
 	public List<Idea> ideas;
-	
+
 	/**
 	 * the list of comments on the topic
 	 */
-	@OneToMany(mappedBy = "commentedTopic" , cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "commentedTopic", cascade = CascadeType.ALL)
 	public List<Comment> commentsOn;
-	
+
 	/**
 	 * the list of requests to join the topic
 	 */
-	//public List<RequestToJoin> requestsToJoin;
-	
+	// public List<RequestToJoin> requestsToJoin;
+
 	/**
 	 * the list of relationship requests with the topic
 	 */
-	//public List<RequestOfRelationship> relationshipRequests;
-	
+	// public List<RequestOfRelationship> relationshipRequests;
+
 	/**
 	 * the list of invitations to the topic?
 	 */
-	//public List<TopicInvitation> topicInvitations;
-	
+	// public List<TopicInvitation> topicInvitations;
+
 	/**
 	 * the entity the topic is in
 	 */
 	@ManyToOne
 	public MainEntity entity;
-	
+
 	/**
 	 * the plan that the topic is promoted to
 	 */
 	@OneToOne
 	public Plan plan;
-	
+
 	/**
 	 * the list of invitations to the topic?
 	 */
 	@OneToMany(mappedBy = "topic", cascade = CascadeType.ALL)
-	 List<Invitation> invitation;
-	
+	List<Invitation> invitation;
+
 	/**
-	 * Default constructor that creates a topic with name, description,privacy level, creator and entity
+	 * Default constructor that creates a topic with name, description,privacy
+	 * level, creator and entity
 	 * 
 	 * @author ${aliaelbolock}
 	 * 
@@ -123,30 +122,31 @@ public class Topic extends Model {
 	 *            title of the topic
 	 * @param description
 	 *            description of the topic
-	 * @param privacyLevel           
+	 * @param privacyLevel
 	 *            the privacy level of the topic
 	 * @param creator
 	 *            Author of the topic
 	 * @param entity
 	 *            Entity that the topic belongs/added to
 	 */
-	public Topic(String title, String description, short privacyLevel, User creator, MainEntity entity) {
+	public Topic(String title, String description, short privacyLevel,
+			User creator, MainEntity entity) {
 		this.title = title;
 		this.description = description;
 		this.privacyLevel = privacyLevel;
 		this.creator = creator;
 		this.entity = entity;
 		tags = new ArrayList<Tag>();
-		//relationships = new ArrayList<Relationship>();
+		// relationships = new ArrayList<Relationship>();
 		organizers = new ArrayList<User>();
 		followers = new ArrayList<User>();
 		ideas = new ArrayList<Idea>();
 		invitation = new ArrayList<Invitation>();
 		commentsOn = new ArrayList<Comment>();
 		canAccess = new ArrayList<User>();
-		//requestsToJoin = new ArrayList<RequestToJoin>();
+		// requestsToJoin = new ArrayList<RequestToJoin>();
 	}
-	
+
 	/**
 	 * Creates a topic with name, description,privacy level and creator
 	 * 
@@ -156,92 +156,94 @@ public class Topic extends Model {
 	 *            title of the topic
 	 * @param description
 	 *            description of the topic
-	 * @param privacyLevel           
+	 * @param privacyLevel
 	 *            the privacy level of the topic
 	 * @param creator
 	 *            Author of the topic
 	 */
-	public Topic(String title, String description, short privacyLevel, User creator) {
+	public Topic(String title, String description, short privacyLevel,
+			User creator) {
 		this.title = title;
 		this.description = description;
 		this.privacyLevel = privacyLevel;
 		this.creator = creator;
 		tags = new ArrayList<Tag>();
-		//relationships = new ArrayList<Relationship>();
+		// relationships = new ArrayList<Relationship>();
 		organizers = new ArrayList<User>();
 		followers = new ArrayList<User>();
 		ideas = new ArrayList<Idea>();
 		invitation = new ArrayList<Invitation>();
 		commentsOn = new ArrayList<Comment>();
-		//requestsToJoin = new ArrayList<RequestToJoin>();
+		// requestsToJoin = new ArrayList<RequestToJoin>();
 	}
-	 
+
 	/**
 	 * This Method returns a list of organizers in a certain topic
-	 *
-	 * @author 	lama.ashraf
-	 *
-	 * @story 	C1S12
-	 *
-	 * @return	ArrayList<User>
+	 * 
+	 * @author lama.ashraf
+	 * 
+	 * @story C1S12
+	 * 
+	 * @return ArrayList<User>
 	 */
-	public List<User> getOrganizer(){
-		 return organizers;
+	public List<User> getOrganizer() {
+		return organizers;
 	}
-	
+
 	/**
 	 * This Method returns the list of ideas in a certain topic
-	 *
-	 * @author 	aliaelbolock
-	 *
-	 * @story 	C3S20
-	 *
-	 * @return	ArrayList<Idea>
+	 * 
+	 * @author aliaelbolock
+	 * 
+	 * @story C3S20
+	 * 
+	 * @return ArrayList<Idea>
 	 */
-	public ArrayList<Idea> getIdeas(){
-		 return (ArrayList<Idea>) ideas;
+	public ArrayList<Idea> getIdeas() {
+		return (ArrayList<Idea>) ideas;
 	}
-	
+
 	/**
 	 * This Method removes a user from the list of followers
 	 * 
-	 * @author 	Ibrahim.al.khayat
+	 * @author Ibrahim.al.khayat
 	 * 
-	 * @story 	C2S12
+	 * @story C2S12
 	 * 
-	 * @param  	user 	: the user who follows
+	 * @param user
+	 *            : the user who follows
 	 * 
-	 * @return	void
+	 * @return void
 	 */
-	
+
 	public void unfollow(User user) {
 		followers.remove(user);
+		_save();
 	}
-	
+
 	/**
 	 * This Method returns the list of followers in a certain topic
 	 * 
-	 * @author 	Omar Faruki
+	 * @author Omar Faruki
 	 * 
-	 * @story 	C2S29
+	 * @story C2S29
 	 * 
-	 * @return	ArrayList<User>
+	 * @return ArrayList<User>
 	 */
 	public ArrayList<User> getFollowers() {
 		return (ArrayList<User>) this.followers;
 	}
-	
-	
+
 	/**
 	 * This Method overrides the toString method
 	 * 
-	 * @author 	aliaelbolock
+	 * @author aliaelbolock
 	 * 
-	 * @story 	C3S1
+	 * @story C3S1
 	 * 
-	 * @return	String
+	 * @return String
 	 */
-	public String toString(){
+	public String toString() {
 		return title;
 	}
 }
