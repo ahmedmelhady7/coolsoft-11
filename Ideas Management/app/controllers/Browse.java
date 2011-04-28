ee6f62f7d245542e4bf91ce281ffdb0edd72cd63
fadzy.amir <fadzy.amir@gmail.com>
2011-04-28 22:53:01 +0000

    fady amir c405: browse browse html page button and return link to the main page. browse.java file that holds browse method which still under construction . and finally creating a button in the main page to go to the browse page

---
+++ Ideas Management/app/controllers/Browse.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.util.ArrayList;
+
+import models.Idea;
+import models.Item;
+import models.MainEntity;
+import models.Organization;
+import models.Tag;
+import models.Topic;
+
+import org.h2.table.Plan;
+
+import play.mvc.Controller;
+
+/**
+ * 
+ * @author Fady Amir
+ * 
+ * @story C4S05 : browse
+ * 
+ */
+public class Browse extends Controller 
+{
+	public static void main() {
+        render();
+    }
+    
+    public static void browse() {
+        render();
+    }
+}
+
