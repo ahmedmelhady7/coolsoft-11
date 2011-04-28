b33d0399629d75bd4e17f711cd3838c0a5549c91
mai.jt4 <mai.jt4@gmail.com>
2011-04-28 21:45:28 +0000

    C1S4 modified the responf method

--- Ideas Management/app/controllers/Invitations.java
+++ Ideas Management/app/controllers/Invitations.java
@@ -7,12 +7,14 @@ import play.mvc.Controller;
 import models.Invitation;
 import models.MainEntity;
 import models.Organization;
+import models.Role;
 import models.Topic;
 import models.User;
+import models.UserRoleInOrganization;
 import notifiers.Mail;
 
 
-public class Invitations extends Controller {
+public class Invitations extends CRUD {
 	
 	 public static void invite(){
 		 
@@ -41,30 +43,53 @@ public class Invitations extends Controller {
 			
 		   //**User user=get user from session
 	       //<Invitation> inv = Invitation.find("byEmail", user.email).fetch();
-	       //render(user,inv);
+	       //render(inv);
 
 		}
 
-	public static void respond(int id,int i){
+	public static void respond(int id,int i,User user){
 		
-		 /**User user=get user from session
+		  //User user=get user from session
 		  List<Invitation> inv = Invitation.find("byEmail", user.email).fetch();
-		  String role=inv.get(i).role;
+		  String rolename=inv.get(i).role;
 		  Organization org=inv.get(i).organization;
 		  MainEntity ent=inv.get(i).entity;
-		  Topic top=inv.get(i).topic;
-		  User sender=inv.get(i).sender;
+		  
+		  Role role=Role.find("byName", rolename).first();
+		  
+		  List <User> organizers= Users.getEntityOrganizers(ent);
+		  List <User> enrolled =Users.getEnrolledUsers(org);
+		  
+		  
+		  boolean flag=false;
+		  for(int j=0;j<organizers.size();j++){
+			  if(organizers.get(j).equals(user))
+				  flag=true;
+		  }
+		  
+		  if(!flag){
+		  UserRoleInOrganizations.addEnrolledUser(user,org,role);
+		  UserRoleInOrganizations.addEnrolledUser(user,org,role,ent.id,"entity");
+		  }
+		  
+		  flag=false;
+		  for(int j=0;j<enrolled.size();j++){
+			  if(enrolled.get(j).equals(user))
+				  flag=true;
+		  }
+		  
+		  if(org.privacyLevel!=2&&flag){
+			  Role rol=Role.find("byName", "Idea Developer").first();
+			  UserRoleInOrganizations.addEnrolledUser(user,org,rol);
+		  }
 		 
 		 
-		   top.invitation.get(i).delete;
-		    org.invitation.get(i).delete;
-		    ent.invitation.get(i).delete;
-		    sender.invitation.get(i).delete;
-		   
-		// flash.success("Invitation(s) sent successfully!");
-		 inv.get(i).delete();
+		 Invitation invite=Invitation.findById(inv.get(i).id);
+		 invite.delete();
 		 
-		 render(id,inv,i);**/
+		 render(id,inv,i);
+		 
+		 
 		
 		
 		
