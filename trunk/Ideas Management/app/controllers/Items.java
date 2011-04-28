660b2247c82cc702b5e58bddc47f5b0c78c80b38
salma.qayed@gmail.com <salma.qayed@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-28 19:46:48 +0000

    C5S11: modified the view C5S10: documented the methods

--- trunk/Ideas Management/app/controllers/Items.java
+++ trunk/Ideas Management/app/controllers/Items.java
@@ -8,11 +8,23 @@ import models.User;
 public class Items extends CRUD {
 
 	
+	/**
+	 * 
+	 * This Method renders the view of the list of items the user is assigned to, given the user id
+	 * 
+	 * @author 	salma.qayed
+	 * 
+	 * @story 	C5S11
+	 * 
+	 * @param 	userid 	: the id of the user whose list of assigned items will be viewed
+	 * 
+	 * @return	void
+	 */
 	
-	public static void viewAssignedItems(long userid) {
-		User user = User.findById(userid);
+	public static void viewAssignedItems(long userId) {
+		User user = User.findById(userId);
 		List<Item> assignedItems = user.itemsAssigned;
-		render(assignedItems);
+		render(assignedItems, userId);
 	}
 
 	public static void startItem(long id) {
@@ -22,17 +34,17 @@ public class Items extends CRUD {
 		}
 	}
 
-//	public static void toggleItem(long id) {
-//		Item item = Item.findById(id);
-//		switch (item.status) {
-//		case 1:
-//			item.status = 2;
-//			break;
-//		case 2:
-//			item.status = 1;
-//			break;
-//		default:
-//			break;
-//		}
-//	}
+	public static void toggleItem(long id) {
+		Item item = Item.findById(id);
+		switch (item.status) {
+		case 1:
+			item.status = 2;
+			break;
+		case 2:
+			item.status = 1;
+			break;
+		default:
+			break;
+		}
+	}
 }
--- trunk/Ideas Management/app/controllers/VolunteerRequests.java
+++ trunk/Ideas Management/app/controllers/VolunteerRequests.java
@@ -8,6 +8,22 @@ import models.VolunteerRequest;
 
 public class VolunteerRequests extends CRUD {
 
+	/**
+	 * 
+	 * This Method creates an instance of VolunteerRequest and adds it to the list of sent volunteer requests of the user
+	 * and to the list of volunteer requests in the item given the user id, the item id and the required justification string
+	 * 
+	 * @author 	salma.qayed
+	 * 
+	 * @story 	C5S10
+	 * 
+	 * @param 	senderId 	: the id of the user sending the volunteer request
+	 * @param 	itemId 	: the id of the item the user wishes to volunteer to work on
+	 * @param   justification: the reason why the user would like to volunteer in this item
+	 * 
+	 * @return	void
+	 */
+	
 	public static void sendVolunteerRequest(long senderId, long itemId,
 			@Required String justification) {
 
@@ -19,6 +35,20 @@ public class VolunteerRequests extends CRUD {
 			sender.addVolunteerRequest(volunteerRequest);
 		}
 
+	/**
+	 * 
+	 * This Method renders the view where the user enters the justification of his volunteer request given the item id and the plan id
+	 * 
+	 * @author 	salma.qayed
+	 * 
+	 * @story 	C5S10
+	 * 
+	 * @param 	planId 	: the id of the plan having the item that the user wants to work on
+	 * @param 	itemId 	: the id of the item the user wishes to volunteer to work on
+	 * 
+	 * @return	void
+	 */
+	
 	public static void justify(long itemId, long planId) {
 		Item item = Item.findById(itemId);
 		
