08994780aeb9e1effaa4bb4c27fdf0fdeba89e0d
hassan.ziko1 <hassan.ziko1@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-28 21:22:07 +0000

    C5S1 implementing the method planCreate

--- trunk/Ideas Management/app/controllers/Plans.java
+++ trunk/Ideas Management/app/controllers/Plans.java
@@ -1,9 +1,12 @@
 package controllers;
 
+import models.Idea;
 import models.Item;
 import models.Plan;
+import models.Topic;
 import models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Plans extends CRUD {
@@ -15,6 +18,21 @@ public class Plans extends CRUD {
 		List<Item> itemsList = p.items;
 
 		render(p, itemsList, user);
+	}
+	public static void planCreate(Topic t, long id){
+		Idea i  = Idea.findById(id);
+		long userid = 0;
+		User user = User.findById(userid);
+		Plan p = new Plan();
+		p.topic = t;
+		p.items = new ArrayList<Item>();
+		p.ideas = new ArrayList<Idea>();
+		p.ideas.add(i);
+		p.requirements = new ArrayList<String>();
+		p.madeBy = user;
+		p.status = "new";
+		
+		render(p, user);
 	}
 
 //	public static void volunteer(long itemid) {
