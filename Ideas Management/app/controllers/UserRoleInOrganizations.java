8997d68d5482768550b11e2d91f92dea341e81a9
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-04-28 15:41:43 +0000

    C1S7 new Controller UserRoleInOrganizations

---
+++ Ideas Management/app/controllers/UserRoleInOrganizations.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.MainEntity;
+import models.Organization;
+import models.User;
+import models.UserRoleInOrganization;
+
+public class UserRoleInOrganizations extends CRUD {
+
+	
+	
+}
