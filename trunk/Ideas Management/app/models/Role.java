bdebb2c032197809e82e6f890611c4f8d560aaff
nada.ossama.kamal@gmail.com <nada.ossama.kamal@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-28 10:25:21 +0000

    C1S7 added method getRoleByName in the Role Model

--- trunk/Ideas Management/app/models/Role.java
+++ trunk/Ideas Management/app/models/Role.java
@@ -1,4 +1,5 @@
 package models;
+
 //Added public to attributes
 import java.util.ArrayList;
 import java.util.List;
@@ -10,28 +11,47 @@ import play.db.jpa.*;
 
 @Entity
 public class Role extends Model {
-	
+
 	public String roleName;
-	
+
 	public ArrayList<String> actions;
 
-//	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
-//	public List<Action> actions;
+	// @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+	// public List<Action> actions;
 
-	@OneToMany(mappedBy = "role" , cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
 	public List<UserRoleInOrganization> userRoleInOrganization;
 
 	public Role(String role) {
 		this.roleName = role;
 		this.actions = new ArrayList<String>();
-		this.userRoleInOrganization = new ArrayList<UserRoleInOrganization> ();
-	
+		this.userRoleInOrganization = new ArrayList<UserRoleInOrganization>();
+
 	}
-	public Role(String role , ArrayList<String> actions) {
+
+	public Role(String role, ArrayList<String> actions) {
 		this.roleName = role;
 		this.actions = actions;
-		this.userRoleInOrganization = new ArrayList<UserRoleInOrganization> ();
-	
+		this.userRoleInOrganization = new ArrayList<UserRoleInOrganization>();
+
+	}
+
+	/*
+	 * retrieve the role by name
+	 * 
+	 * @author Nada Ossama
+	 * 
+	 * @story C1S7
+	 * 
+	 * @pram: role is the name of the role
+	 * 
+	 * @return Role
+	 */
+
+	public static Role getRoleByName(String role) {
+
+		return Role.find("byRoleName", role).first();
+
 	}
 
 }
