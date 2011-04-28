c079c9041bb79cda5a03f6f3478d739c7711e718
mai.jt4 <mai.jt4@gmail.com>
2011-04-28 23:13:14 +0000

    C1S4 C1S6 documentations

--- Ideas Management/app/controllers/Invitations.java
+++ Ideas Management/app/controllers/Invitations.java
@@ -16,28 +16,92 @@ import notifiers.Mail;
 
 public class Invitations extends CRUD {
 	
+	
+	 /**
+	 * 
+	 * This method is responsible for 
+	 *
+	 * 
+	 * @author ${Mai.Magdy}
+	 * 
+	 * @story C1S6
+	 * 
+	 * @param 
+	 * 
+	 *                 
+	 * @param 
+	 *             
+	 *               
+	 *@param
+	 *               
+	 *               
+	 *@param
+	 *              
+	 *                                
+	 * @return
+	 */
+ 
+	
 	 public static void invite(){
 		 
 		      /*get user n list of organization&entity&topic tht he organizes*/
 	    	//render(org,ent,top);
 	  }
 	 
-	
+	 /**
+		 * 
+		 * This method is responsible for sending an invitation
+		 *
+		 * 
+		 * @author ${Mai.Magdy}
+		 * 
+		 * @story C1S6
+		 *
+		 * 
+		 * @param  email
+		 *                 destination of the invitation
+		 *                 
+		 * @param  role
+		 *               role that ll be assigned to the user in case accepted
+		 *               
+		 *@param  organization
+		 *                organization that sends the invitation
+		 *               
+		 *@param  entity
+		 *               entity that sends the invitation
+		 *                                
+		 * @return
+		 */
+	 
 	 public static void send(String email,String role,String organization,
-			 String entity,String topic, String Comments){
+			 String entity){
 		  
-		 Mail.invite(email,role,organization,entity,topic);
+		 Mail.invite(email,role,organization,entity);
 		    
 	    	Organization org= Organization.find("byName", organization).first();
 	    	MainEntity ent=MainEntity.find("byName", entity).first();
-	    	Topic top=Topic.find("byName", topic).first();
-	    	 //**sender=the user from the session 
-	        //**sender.addInvitation(email,role,org,ent,top);
+	    	 
+	    	//sender=the user from the session 
+	        //sender.addInvitation(email,role,org,ent,top);
 	    	
 		  //render(email,role,organization,entity,topic);
 		    render(email);
 	  }
 	 
+	 /**
+		 * 
+		 * This method is responsible for viewing the received invitations
+		 *
+		 * 
+		 * @author ${Mai.Magdy}
+		 * 
+		 * @story C1S4
+		 * 
+		 * @param  
+		 *       
+		 * 
+		 * @return
+		 */
 	 
 	 public static void view(){
 			
@@ -46,6 +110,27 @@ public class Invitations extends CRUD {
 	       //render(inv);
 
 		}
+	 
+	 
+	 /**
+		 * 
+		 * This method is responsible for responding to the user (accept/reject)
+		 * to the invitation
+		 *
+		 * 
+		 * @author ${Mai.Magdy}
+		 * 
+		 * @story C1S4
+		 * 
+		 * @param id  
+		 *              0 if reject or 1 if accept
+		 * 
+		 * @param id  
+		 *              the number of the invitation in the list
+		 *            
+		 * 
+		 * @return
+		 */
 
 	public static void respond(int id,int i,User user){
 		
@@ -71,7 +156,7 @@ public class Invitations extends CRUD {
 		  UserRoleInOrganizations.addEnrolledUser(user,org,role);
 		  UserRoleInOrganizations.addEnrolledUser(user,org,role,ent.id,"entity");
 		  }
-		  
+		  /*
 		  flag=false;
 		  for(int j=0;j<enrolled.size();j++){
 			  if(enrolled.get(j).equals(user))
@@ -81,7 +166,7 @@ public class Invitations extends CRUD {
 		  if(org.privacyLevel!=2&&flag){
 			  Role rol=Role.find("byName", "Idea Developer").first();
 			  UserRoleInOrganizations.addEnrolledUser(user,org,rol);
-		  }
+		  }*/
 		 
 		 
 		 Invitation invite=Invitation.findById(inv.get(i).id);
