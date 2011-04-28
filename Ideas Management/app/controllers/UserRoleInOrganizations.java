bfbced6d399755914ed98b565440ad380ae1757a
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-04-28 21:18:07 +0000

    C1S7 moved the method from userRole in Organization to UserRoleInOrganizations and to Users

--- Ideas Management/app/controllers/UserRoleInOrganizations.java
+++ Ideas Management/app/controllers/UserRoleInOrganizations.java
@@ -5,11 +5,66 @@ import java.util.List;
 
 import models.MainEntity;
 import models.Organization;
+import models.Role;
 import models.User;
 import models.UserRoleInOrganization;
 
 public class UserRoleInOrganizations extends CRUD {
 
 	
+	/*
+	 * This method adds a new enrolled user in the organization where his role
+	 * is NOT related to a specific topic, entity .. etc
+	 * 
+	 * @author Nada Ossama
+	 * 
+	 * @story :C1S7
+	 * 
+	 * @param user: is the enrolled user
+	 * 
+	 * @param org: is the organization the User user is enrolled in
+	 * 
+	 * @param role: the role of that user in this organization
+	 * 
+	 * return boolean indicating the successfulness of the operation
+	 */
+
+	public static boolean addEnrolledUser(User user, Organization org, Role role) {
+		new UserRoleInOrganization(user, org, role).save();
+
+		return true;
+	}
 	
+	/*
+	 * This method adds a new enrolled user in the organization where his role
+	 * is related to a specific topic, entity .. etc
+	 * 
+	 * @author Nada Ossama
+	 * 
+	 * @story :C1S7
+	 * 
+	 * @parm user: is the enrolled user
+	 * 
+	 * @parm org: is the organization the User user is enrolled in
+	 * 
+	 * @parm role: the role of that user in this organization
+	 * 
+	 * @parm entityOrTopicId : the id of the entity or topic the role of that
+	 * user is related to
+	 * 
+	 * @parm type: the type (entity / topic)
+	 * 
+	 * return boolean indicating the successfulness of the operation
+	 */
+
+	public static boolean addEnrolledUser(User user, Organization org,
+			Role role, long entityOrTopicId, String type) {
+		new UserRoleInOrganization(user, org, role, entityOrTopicId, type)
+				.save();
+
+		return true;
+	}
+
+	
+
 }
