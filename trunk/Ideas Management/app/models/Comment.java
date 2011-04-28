362be690e19bd3f8c577ebd33e644f6043a66173
ibrahim.yehia.safwat@gmail.com <ibrahim.yehia.safwat@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-28 13:54:16 +0000

    Added methods:- addCommentToIdea and addCommentToPlan

--- trunk/Ideas Management/app/models/Comment.java
+++ trunk/Ideas Management/app/models/Comment.java
@@ -36,5 +36,31 @@ public class Comment extends Model {
 		this.postedOn=postedOn;
 		//this.commenter=commenter;
 	}
+	
+	/**
+	 * @author ${Ibrahim safwat}
+	 * 
+	 * @param ideaID
+	 * 			ID of the idea that the user wants to add the comment to
+	 * @param comment
+	 * 			Comment to be added to list of comments of the idea
+	 */
+	public void addCommentToIdea(int ideaID, String comment)
+	{
+		
+	}
+	
+	/**
+	 * @author ${Ibrahim safwat}
+	 * 
+	 * @param planID
+	 * 			ID of the plan that the user wants to add the comment to
+	 * @param comment
+	 * 			Comment to be added to list of comments of the plan
+	 */
+	public void addCommentToPlan(int planID, String comment)
+	{
+		
+	}
 
 }
