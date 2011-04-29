f063b132ff6e6c9644c1145956d4295991eaf789
elhadiahmed3 <elhadiahmed3@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-29 19:19:41 +0000

    C3S12 finished both methods ReportAsSpamMail in the Mail class and ReportAsSpam in the Users class and created an empty html file the mail method

--- Ideas Management/app/notifiers/Mail.java
+++ Ideas Management/app/notifiers/Mail.java
@@ -1,30 +1,47 @@
+package notifiers;
 
-	package notifiers;
+import play.mvc.*;
+import java.util.*;
 
-	import play.mvc.*;
-	import java.util.*;
+import models.*;
 
-	import models.Organization;
-import models.User;
+public class Mail extends Mailer {
 
-	public class Mail extends Mailer{
+	public static void invite(String email, String role, String organization,
+			String entity) {
+		addRecipient(email);
+		setFrom("CoolSoft011@gmail.com");
+		setSubject("Invitation");
+		// String Link = "" ;
+		User user = User.find("byEmail", email).first();
+		send(user, role, organization, entity);
 
+	}
 
-		public static void invite(String email,String role,String organization,String entity)
-		{
-			addRecipient(email);
-			setFrom("CoolSoft011@gmail.com");
-			setSubject("Invitation");
-			//String Link = "" ;
-			User user=User.find("byEmail", email).first();
-			send(user,role,organization,entity);
-	        
-	        
-	        
-		
+	/*
+	 * this Method is responsible for sending a mail to the topic organizers
+	 * when an idea inside the topic is reported as a spam
+	 * 
+	 * @author ${Ahmed El-Hadi}
+	 * 
+	 * @story C3S12
+	 * 
+	 * @param topicOrganizer : the organizer of be topic
+	 * 
+	 * @param reporter : the user who reported the idea
+	 * 
+	 * @param reportedIdea : the reported idea
+	 */
 
-}
-		
-
-		
+	public static void ReportAsSpamMail(User topicOrganizer, User reporter,
+			Idea reportedIdea) {
+		String email = "The idea with this title " + reportedIdea.title
+				+ " and with this description " + reportedIdea.description
+				+ " has been reported as a spam by this user "
+				+ reporter.username;
+		addRecipient(email);
+		setFrom("coolsoft-11@gmail.com");
+		setSubject("An Idea is Reported as a Spam");
+		send(topicOrganizer);
+	}
 }
