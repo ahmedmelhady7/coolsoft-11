55da5d98b93074bccd47331adba1e18849689fe4
ibrahim.yehia.safwat@gmail.com <ibrahim.yehia.safwat@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-04-29 14:39:25 +0000

    Added methods to idea model:- -rate -setPriority -shareIdea Added attribute to idea model:- commentList

--- trunk/Ideas Management/app/models/Idea.java
+++ trunk/Ideas Management/app/models/Idea.java
@@ -76,8 +76,23 @@ public class Idea extends Model {
 
 	public boolean isDraft;
 
+	/**
+	 * @author ${Ibrahim Safwat}
+	 * Each idea can have a rating
+	 */
 	public int rating;
+	
+	/**
+	 * @author ${Ibrahim Safwat}
+	 * Must keep track of which users rated
+	 */
+	//@OneToOne (mappedBy = "idea", cascade = CascadeType.ALL)
+	//public List<User> usersRated;
 
+	/**
+	 * @author ${Ibrahim Safwat}
+	 * The organizer can prioritize ideas
+	 */
 	public String priority; // priority could equal
 							// "Critical"/"High"/"Medium"/"Low
 
@@ -136,6 +151,45 @@ public class Idea extends Model {
 		this.isDraft = isDraft;
 		this.tagsList = new ArrayList<Tag>();
 		// this.commentsList = new ArrayList<Comment>();
+	}
+	
+	/**
+	 * @author ${Ibrahim safwat}
+	 * 
+	 * @param UserToShare
+	 * 				User that wants to share the idea
+	 * @param UserToShareWith
+	 * 				User the will be sent the notification with the ideaID
+	 * @param ideaID
+	 * 				ID of the idea to be shared
+	 */
+	public void shareIdea(User UserToShare, User UserToShareWith, int ideaID)
+	{
+		// send a notification to UserToShareWith with the ideaID from UserToShare
+	}
+	
+	public void setPriority(String priority, int ideaID)
+	{
+		//user must be organizer
+		//ideaID.priority = priority
+	}
+	
+	public void rate(int rate, int ideaID)
+	{
+		//user must be organizer in the organization to rate an idea
+		//if user already rated -> error "already rated"
+		
+//		if(rate>=0 && rate<=5)
+//		{
+//			float oldRating;
+//			float tempRating;
+//			tempRating = (oldRating + rate)/2;
+//			ideaID.rating = tempRating;
+//		}
+//		else
+//		{
+//			error"Number must be between 0 and 5"
+//		}
 	}
 
 }
