e3679b0a7851b0e0159bb2f4bfe4b232f59868dc
aliaelbolock <aliaelbolock@gmail.com>
2011-04-29 00:44:08 +0000

    C3S1 description: deleted postTopic method from Users

--- Ideas Management/app/controllers/Users.java
+++ Ideas Management/app/controllers/Users.java
@@ -207,54 +207,7 @@ public class Users extends CRUD {
 		}else
 			return;
 	}
-	/**
-	 * 
-	 * This method is responsible for posting a topic in a certain entity by a
-	 * certain user
-	 * 
-	 * @author ${aliaelbolock}
-	 * 
-	 * @story C3S1
-	 * 
-	 * @param name
-	 *            : the title of the topic to be posted/posted
-	 * 
-	 * @param description
-	 *            : the description of the topic to be added/posted
-	 * 
-	 * @param privacyLevel
-	 *            : the privacy level of the topic
-	 * 
-	 * @param creator
-	 *            : the user who added/posted the topic
-	 * 
-	 * @param entity
-	 *            : the entity in which the topic will be added/posted
-	 * 
-	 * @return void
-	 */
-	public void postTopic(String name, String description, short privacyLevel,
-			User creator, MainEntity entity) {
-
-		if (entity.organizers.contains(creator)) {
-			Topic newTopic = new Topic(name, description, privacyLevel,
-					creator, entity).save();
-			creator.topicsCreated.add(newTopic);
-
-//			List usr = getEntityOrganizers(entity); 
-//			UserRoleInOrganization.addEnrolledUser(creator,
-//					newTopic.entity.organization,
-//					Roles.getRoleByName("Organizer"), newTopic.getId(), "topic");
-			List usr = getEntityOrganizers(entity);
-			// UserRoleInOrganization.addEnrolledUser(creator,
-			// newTopic.entity.organization,
-			// Role.getRoleByName("Organizer"), newTopic.getId(), "topic");
-			creator.topicsIOrganize.add(newTopic);
-			for (int i = 0; i < entity.organizers.size(); i++) {
-				entity.organizers.get(i).topicsIOrganize.add(newTopic);
-			}
-		}
-	}
+	
 
 	/**
 	 * 
