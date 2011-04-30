74cddff857afd7fd187a73ca1fbe47a6a9b6be22
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-04-30 19:47:44 +0000

    C1S7 added method getOrganizer Topic Action to it

--- Ideas Management/app/controllers/Roles.java
+++ Ideas Management/app/controllers/Roles.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import play.*;
 import models.*;
 
@@ -22,49 +25,31 @@ public class Roles extends CRUD{
 		return Role.find("byRoleName", role).first();
 
 	}
-	
 	/**
-	 * method that creates the Organizer role
-	 * @ author nada ossama
-	 * 
+	 * This methods returns all the DEFAULT actions permitted to a certain role
+	 * @author nada ossama
+	 * @param roleName is the name of the role
+	 * @return list of actions of that role
 	 */
-	public static void createOrganizerRole(){
+	
+   public static List<String> getRoleActions (String roleName){
 		
-		String [] role =  new String []{
-			    "invite Organizer or Idea Developer to become Organizer or Idea Developer in an entity he/she manages",
-				"block a user from viewing or using a certain entity",
-				"Request to start a relationship with other items",
-				"Request to end a relationship with other items",
-				"Accept/Reject a request to start/end a relationship with other items",
-				"Create a sub-entity for entity he/she manages", 
-				"Accept/Reject requests to post in a private topic in entities he/she manages",
-				"Request to rename relationships that are related to any of the entities or topics he/she manages",
-				"Accept/Reject request to rename relationships that are related to any of the entities or topics I am managing",
-				"Create a certain topic and specify its type within entities that he/she manages",
-				"Receive notifications about any changes or updates within the entities that he/she created or he/she currently manages",
-				"Invite a user to join a private or secret organization",
-				"Add images and documents to the organization profile",
-				"post topics",
-				"tag topics",
-				"tag ideas in my organization",
-				"close a topic and promote it to execution",
-				"**delete other ideas",
-				"merge ideas",
-				"**hide and delete an idea",
-				"delete and hide topics that have no ideas posted on them",
-				"rate/prioritize ideas",
-				"receive notifications about inactive ideas",
-				"confirm or reject marking requests",
-				"view the list of ideas",
-				"create an action plan to execute an idea",
-				"delete an action plan",
-				"edit an action plan",
-				"associate an idea or more to an already existing plan",
-				"assign one or many users to a to-do item in a plan",
-				"accept/Reject user request to volunteer to work on action item in a plan"};
-		Role Organizer  = new Role("organizer" , role);
-		Organizer.save();
-
+		return (List<String>) (Role.find("select r.actions from Role r "
+							+ "where r.roleName like ? ",roleName ).first());
 	}
-
+   //13
+	/**
+	 * this method returns the list of Organizer actions that are related to Topics not the whole entity
+	 * @author nada  ossama
+	 * @param 
+	 */
+	public static List <String> getOrganizerTopicActions (){
+		
+		List<String> allActions = getRoleActions("organizer");
+		List<String> topicActions = new ArrayList<String>();
+		for (int i = 13 ; i < allActions.size() ; i++ ){
+		 topicActions.add(allActions.get(i));
+		}
+		return topicActions;
+	}
 }
