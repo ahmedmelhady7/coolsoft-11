b82edcf763837d2075003170bb9ff70fb06612a9
salma.qayed@gmail.com <salma.qayed@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-01 10:03:36 +0000

    added back some methods to Plans class that were somehow lost when someone commited

--- trunk/Ideas Management/app/controllers/Plans.java
+++ trunk/Ideas Management/app/controllers/Plans.java
@@ -19,14 +19,14 @@ import controllers.CRUD.ObjectType;
 
 public class Plans extends CRUD {
 	
-	public static void viewAsList(long planId) {
-		long userid = 0;
-		User user = User.findById(userid);
-		Plan p = Plan.findById(planId);
-		List<Item> itemsList = p.items;
+	  public static void viewAsList(long planId, String y) {
+          long userid = 0;
+          User user = User.findById(userid);
+          Plan p = Plan.findById(planId);
+          List<Item> itemsList = p.items;
 
-		render(p, itemsList, user);
-	}
+          render(p, itemsList, user, y);
+  }
 
 	
 	
@@ -170,21 +170,78 @@ public class Plans extends CRUD {
 		p.requirement = requirement;
 		p.save();
 	}
-//	public static void volunteer(long itemid) {
-//		long userid = 0;
-//		User user = User.findById(userid);
-//		Item item = Item.findById(itemid);
-//		int y = 0;
-//		if (item.assignees.contains(user)) {
-//			y = 1;
-//			render();
-//		} 
-//		
-//
-//	}
-//
-//	public static void assign(long itemid) {
-//
-//	}
 
+  public static void volunteer(long itemId, long planId) {
+          long userid = 0;
+          User user = User.findById(userid);
+          Item item = Item.findById(itemId);
+          String y = "";
+          if (item.assignees.contains(user)) {
+
+                  y = "You are already assigned to this item";
+          } else {
+
+                  for (int i = 0; i < item.volunteerRequests.size(); i = i + 1) {
+                          if (user.id == item.volunteerRequests.get(i).sender.id) {
+                                  y = "You already sent a volunteer request to work on this item";
+                          }
+                  }
+
+                  for (int i = 0; i < item.assignRequests.size(); i = i + 1) {
+                          if (user.id == item.assignRequests.get(i).destination.id) {
+                                  y = "You already received an assignment request to work on this item";
+                          }
+                  }
+          }
+          if (y == "") {
+                  VolunteerRequests.justify(itemId, planId);
+          } else {
+                  viewAsList(planId, y);
+          }
+  }
+
+
+  public static void viewAsTimeline(long planid) {
+          // Plan p = Plan.findById(planid);
+          // List<Item> itemsList = p.items;
+          // render(p, itemsList);
+          render();
+  }
+  /*
+   * @author yassmeen.hussein
+   * 
+   * this method renders the Calendar view of a plan
+   * 
+   * @param planId 
+   *       the id of the plan
+   * 
+   * @return void
+   * 
+   * */
+  
+  public static void viewAsCalendar(long planId) {
+
+          Plan p = Plan.findById(planId);
+          List<Item> itemsList = p.items;
+          long userid = 0;
+          User user = User.findById(userid);
+          render(p, itemsList, user);
+  }
+  
+//	public static void volunteer(long itemid) {
+//	long userid = 0;
+//	User user = User.findById(userid);
+//	Item item = Item.findById(itemid);
+//	int y = 0;
+//	if (item.assignees.contains(user)) {
+//		y = 1;
+//		render();
+//	} 
+//	
+//
+//}
+//
+//public static void assign(long itemid) {
+//
+//}
 }
