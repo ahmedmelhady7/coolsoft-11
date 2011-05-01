4e7792f42a5e8f61e4af609c765a095033298cf8
nohakhater@gmail.com <nohakhater@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-01 21:51:53 +0000

    C2S10: Re-added the followTopic() method

--- trunk/Ideas Management/app/controllers/Topics.java
+++ trunk/Ideas Management/app/controllers/Topics.java
@@ -907,5 +907,29 @@ public class Topics extends CRUD {
 		redirect(request.controller + ".show", ((Topic) object).getId());
 		System.out.println("save() done, redirected to default show.html");
 	}
+	
+	/**
+	 * The method that allows a user to follow a certain topic
+	 * 
+	 * @author Noha Khater
+	 * 
+	 * @Stroy  C2S10
+	 * 
+	 * @param topicId
+	 * 				: The id of the topic that the user wants to follow 
+	 * 
+	 * @param user
+	 * 				: The user who wants to follow a topic
+	 */
+	
+	public static void followTopic(long topicId ,User user) {
+		Topic t = Topic.findById(topicId);
+		if(Users.isPermitted(user, "follow", topicId, "topic")){
+			t.followers.add(user);
+			user.topicsIFollow.add(t);
+		} else {
+			System.out.println("Sorry! Action cannot be performed");
+		}
+	}
 
 }
