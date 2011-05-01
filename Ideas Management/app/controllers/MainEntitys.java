9ab39a9afacb2d1b36f52ba1a6ea08ef4e2afb3e
nohakhater <nohakhater@gmail.com>
2011-05-01 02:53:25 +0000

    C2S10: Added the method which allows a user to follow an entity.

--- Ideas Management/app/controllers/MainEntitys.java
+++ Ideas Management/app/controllers/MainEntitys.java
@@ -1,8 +1,33 @@
 package controllers;
 
 import models.MainEntity;
+import models.Topic;
 import models.User;
 
 public class MainEntitys extends CRUD {
+	
+	/**
+	 * The method that allows a user to follow a certain entity
+	 * 
+	 * @author Noha Khater
+	 * 
+	 * @Stroy  C2S10
+	 * 
+	 * @param entityId
+	 * 				: The id of the entity that the user wants to follow 
+	 * 
+	 * @param user
+	 * 				: The user who wants to follow an entity
+	 */
+	
+	public void followEntity(long entityId ,User user) {
+		MainEntity e = MainEntity.findById(entityId);
+		if(Users.isPermitted(user, "follow", entityId, "entity")) {
+			e.followers.add(user);
+			user.followingEntities.add(e);
+		} else {
+			System.out.println("Sorry! Action cannot be performed");
+		}
+	}
 
 }
