80c7fb781e52e91767389bb3ffdc7abe0b07ce21
aymaestro <aymaestro@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-01 21:34:45 +0000

    C1S14: I modified the conf/routes file to render the Login.index upon getting the /, and documented all the methods in the Notifications and NotificationProfiles classes.

--- Ideas Management/app/controllers/NotificationProfiles.java
+++ Ideas Management/app/controllers/NotificationProfiles.java
@@ -6,7 +6,25 @@ import models.NotificationProfile;
 import models.User;
 import play.mvc.Controller;
 
+/**
+ * This class is the notification profile's controller.
+ * 
+ * @author Ahmed Maged
+ *
+ */
+
 public class NotificationProfiles extends Controller {
+	
+	/**
+	 * This method renders the view were the user can change his preferences.
+	 * 
+	 * @author Ahmed Maged
+	 * 
+	 * @story C1S14
+	 * 
+	 * @return void
+	 * 
+	 */
 
 	public static void alterPreferences() {
 		List<User> ul = User.findAll();
@@ -15,16 +33,26 @@ public class NotificationProfiles extends Controller {
 		// String x = u.username;
 		render(npList);
 	}
+	
+	/**
+	 * This method changes the preferences of the user according to the list
+	 * provided as input to the method.
+	 * 
+	 * @author Ahmed Maged
+	 * 
+	 * @story C1S14
+	 * 
+	 * @param a
+	 * 		The list of notification sources to be disabled.
+	 * 
+	 * @return void
+	 * 
+	 */
 
-	public static void changePreferences(int[] a) {
+	public static void changePreferences(long[] a) {
 		for (int i = 0; i < a.length; i++) {
 			NotificationProfile np = NotificationProfile.findById(a[i]);
 			np.disableNotification();
 		}
-	}
-
-	public static void disablePreference(long nId) {
-		NotificationProfile np = NotificationProfile.findById(nId);
-		np.disableNotification();
 	}
 }
