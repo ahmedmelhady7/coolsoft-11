fd65e913f72347c595a87351257d6d5eb1c2f3ad
ibrahim.al.khayat@gmail.com <ibrahim.al.khayat@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-01 10:53:35 +0000

    fixed the bootstrap location

---
+++ trunk/Ideas Management/app/Bootstrap.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+
+import play.*;
+import play.jobs.*;
+import play.test.*;
+
+import models.*;
+
+@OnApplicationStart
+public class Bootstrap extends Job {
+
+	public void doJob() {
+		
+		User u1 = new User("h@gmail.com", "1234", "Hamed", "EL-Akhdar",
+				"hamedcool", 0, new Date(), "egypt", "student");
+		u1._save();
+		Organization org1 = new Organization("HamedSoft", u1, (short) 0, true);
+		org1._save();
+		MainEntity me1 = new MainEntity("Hamed yntlek",
+				"ideas to help hamed become like billgates", org1);
+		me1._save();
+		Topic t1 = new Topic("What to do?", "Help me please", (short) 0, u1,
+				me1);
+		t1._save();
+		Tag tag = new Tag("Sports");
+		tag._save();
+		RequestToJoin request = new RequestToJoin(u1, t1, org1, "I would like to join your organization..");
+		request._save();
+	}
+}
