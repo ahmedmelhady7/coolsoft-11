54b35904a4e7f4ca3c58e5d791e933a55cfe29e5
aymaestro <aymaestro@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-04 14:59:29 +0000

    C1S14: added further modifications and tested with many test cases

--- trunk/Ideas Management/app/controllers/Notifications.java
+++ trunk/Ideas Management/app/controllers/Notifications.java
@@ -82,4 +82,37 @@ public class Notifications extends CRUD {
 		}
 		return false;
 	}
+	
+	public static void sendNotify(String directed, long notId,
+			String type, String desc) {
+		User u = User.find("byUsername", directed).first();
+		List<NotificationProfile> np = u.notificationProfiles;
+		boolean contains = false;
+		boolean isenabled = true;
+		/**
+		 * check that the notification profile has the sending source
+		 * if not then add it, if present the check if enabled or not.
+		 */
+		for (int i = 0; i < np.size(); i++) {
+			if(np.get(i).notifiableId == notId && np.get(i).notifiableType.equals(type)) {
+				isenabled = np.get(i).enabled;
+				contains = true;
+				break;
+			}
+		}
+		// Adding the NP if not there
+		if(!contains) {
+			NotificationProfile np2 = new NotificationProfile(notId, type, u);
+			np2.save();
+			//u.notificationProfiles.add(np2);
+			isenabled = true;
+		}
+		// Send the notification if enabled
+		if(isenabled) {
+			Notification n = new Notification(type, u, desc);
+			n.save();
+		}
+		render();
+	}
+
 }
