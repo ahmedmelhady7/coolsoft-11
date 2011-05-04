52ea058e5adb74f96a33ef32e6539124f9563855
ibrahim.al.khayat@gmail.com <ibrahim.al.khayat@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-04 14:24:51 +0000

    Quick fix

--- trunk/Ideas Management/app/models/Relationship.java
+++ trunk/Ideas Management/app/models/Relationship.java
@@ -1,50 +1,50 @@
-package models;
-
-import java.util.Date;
-
-import play.db.jpa.Model;
-
-/**
- * This class defines the Relationship between two Entities/Topics/Tags
- * 
- * @author Mohamed Hisham
- * 
- */
-public class Relationship extends Model {
-
-	/**
-	 * the name of the Relationship
-	 */
-	public String name;
-
-	/**
-	 * the first related Entity/Topic/Tag
-	 */
-	public Object source;
-
-	/**
-	 * the second related Entity/Topic/Tag
-	 */
-	public Object destination;
-
-	/**
-	 * The constructor for assigning Relationships
-	 * 
-	 * @author Mohamed Hisham
-	 * 
-	 * @param name
-	 *            : the name of the Relationship
-	 * 
-	 * @param source
-	 *            : the first related Entity/Topic/Tag
-	 * 
-	 * @param destination
-	 *            : the second related Entity/Topic/Tag
-	 */
-	public Relationship(String name, Object source, Object destination) {
-		this.name = name;
-		this.source = source;
-		this.destination = destination;
-	}
-
-}
+//package models;
+//
+//import java.util.Date;
+//
+//import play.db.jpa.Model;
+//
+///**
+// * This class defines the Relationship between two Entities/Topics/Tags
+// * 
+// * @author Mohamed Hisham
+// * 
+// */
+//public class Relationship extends Model {
+//
+//	/**
+//	 * the name of the Relationship
+//	 */
+//	public String name;
+//
+//	/**
+//	 * the first related Entity/Topic/Tag
+//	 */
+//	public Object source;
+//
+//	/**
+//	 * the second related Entity/Topic/Tag
+//	 */
+//	public Object destination;
+//
+//	/**
+//	 * The constructor for assigning Relationships
+//	 * 
+//	 * @author Mohamed Hisham
+//	 * 
+//	 * @param name
+//	 *            : the name of the Relationship
+//	 * 
+//	 * @param source
+//	 *            : the first related Entity/Topic/Tag
+//	 * 
+//	 * @param destination
+//	 *            : the second related Entity/Topic/Tag
+//	 */
+//	public Relationship(String name, Object source, Object destination) {
+//		this.name = name;
+//		this.source = source;
+//		this.destination = destination;
+//	}
+//
+//}
