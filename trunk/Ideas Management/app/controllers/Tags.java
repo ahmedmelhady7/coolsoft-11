681db68eacddcd7ad9e78e41c75e4d9a90aa6880
omarmafaruki@gmail.com <omarmafaruki@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-04 17:20:48 +0000

    Tags controller updated

--- trunk/Ideas Management/app/controllers/Tags.java
+++ trunk/Ideas Management/app/controllers/Tags.java
@@ -1,10 +1,15 @@
 package controllers;
 
+import java.util.List;
+
 import play.data.validation.Required;
 import play.mvc.Controller;
+import models.Idea;
+import models.Item;
 import models.MainEntity;
 import models.Organization;
 import models.Tag;
+import models.Topic;
 import models.User;
 
 public class Tags extends CRUD {
@@ -18,42 +23,36 @@ public class Tags extends CRUD {
 	 * @param name
 	 *            : name of the tag that is going to be created
 	 * 
-	 * @param orgId
-	 *            : the current organization that the user is enrolled in
+	 * @param orgId 
+	 * 				: the current organization that the user is enrolled in
 	 */
+
 
 	public static void createTag(@Required String name, long orgId) {
 		Organization org = Organization.findById(orgId);
 		if (validation.hasErrors()) {
 			params.flash();
 			validation.keep();
-			render(org.createTag);
+			render(org);
 		}
 		Tag existing_tag = Tag.find("name like '" + name + "'").first();
 		if (existing_tag != null) {
 			flash.error("Tag already exists!" + "\n\t\t"
 					+ "Please choose another tag name.");
-			render(org.createTag);
+			render(org);
 		}
-		Tag tag = new Tag(name, org);
+		Tag tag = new Tag(name,org);
 		tag.save();
-
+		
 		flash.success("Your tag has been created.");
 	}
-
-	/**
-	 * This method adds tag2 to the list of tags in tag
-	 * 
-	 * @author Mohamed Hisham
-	 * 
-	 * @param tag
-	 *            : first tag to be related
-	 * 
-	 * @param tag2
-	 *            : second tag to be related
-	 */
-	public static void relateTag(Tag tag, Tag tag2) {
-//		tag.relatedTags.add(tag2);
+	public static void mainPage(long tagId) {
+		Tag tag = Tag.findById(tagId);
+		List<User> followers = tag.followers;
+		List<Topic> topics = tag.taggedTopics;
+		List<Organization> organizations = tag.organizations;
+		List<MainEntity> entities = tag.entities;
+		List<Idea> ideas = tag.taggedIdeas;
+		render(tag,followers,topics,organizations,entities,ideas);
 	}
-
 }
