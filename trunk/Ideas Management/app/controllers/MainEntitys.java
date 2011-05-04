a0fe25db4a1684560cdccf3c84249b795a3c595f
nohakhater@gmail.com <nohakhater@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-04 03:35:49 +0000

    C2S10: Edited the followEntity method by removing the user from the parameters list Added the methods for the views: createEntity and viewEntity

--- trunk/Ideas Management/app/controllers/MainEntitys.java
+++ trunk/Ideas Management/app/controllers/MainEntitys.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import java.util.List;
+
+import play.data.validation.Required;
 import models.MainEntity;
+import models.Organization;
 import models.Topic;
 import models.User;
 
@@ -16,11 +20,10 @@ public class MainEntitys extends CRUD {
 	 * @param entityId
 	 *            : The id of the entity that the user wants to follow
 	 * 
-	 * @param user
-	 *            : The user who wants to follow an entity
 	 */
 
-	public static void followEntity(long entityId, User user) {
+	public static void followEntity(long entityId) {
+		User user = Security.getConnected();
 		MainEntity e = MainEntity.findById(entityId);
 		if (Users.isPermitted(user, "follow", entityId, "entity")) {
 			e.followers.add(user);
@@ -29,5 +32,40 @@ public class MainEntitys extends CRUD {
 			System.out.println("Sorry! Action cannot be performed");
 		}
 	}
-
+	
+	public static void createEntity(@Required String name, @Required String desc,
+			Organization org) {
+		if(validation.hasErrors()) {
+			params.flash();
+			validation.keep();
+			render(name, desc);
+		}
+		MainEntity existing = MainEntity.find(
+				"name like '" + name + "'").first();
+		if (existing != null) {
+			flash.error("Entity already exists!" + "\n\t\t"
+					+ "Please choose another organization name.");
+			render(name, desc);
+		}
+		MainEntity e = new MainEntity(name, desc, org);
+		e.save();
+		flash.success("Your entity has been created.");
+	}
+	
+//	public static void createEntity(@Required String n, 
+//			@Required String d, Organization org) {
+//		MainEntity entity = new MainEntity(n, d, org);
+//		entity.save();
+//		flash.success("Done =)");
+//		
+//	}
+	
+	public static void viewEntity(long id) {
+		User user = Security.getConnected();
+		MainEntity entity = MainEntity.findById(id);
+		Organization org = entity.organization;
+		List<MainEntity> subentities = entity.subentities;
+		render(user, org, entity, subentities);
+	}
 }
+
