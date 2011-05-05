84dc32f1ff571215e1211e6ada1164216ec6dfc0
ibrahim.yehia.safwat@gmail.com <ibrahim.yehia.safwat@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-05 11:52:13 +0000

    Moved methods from comment model to controller

--- trunk/Ideas Management/app/controllers/Comments.java
+++ trunk/Ideas Management/app/controllers/Comments.java
@@ -3,10 +3,39 @@
  */
 package controllers;
 
+import models.Comment;
+import models.Idea;
+import models.Plan;
+
 /**
  * @author ${Ahmed El-Hadi}
  *
  */
 public class Comments extends CRUD {
+	
+	/**
+	 * @author ${Ibrahim safwat}
+	 * 
+	 * @param planID
+	 *            ID of the plan that the user wants to add the comment to
+	 * @param comment
+	 *            Comment to be added to list of comments of the plan
+	 */
+	public void addCommentToPlan(long planID, Comment comment) {
+		Plan p = Plan.findById(planID);
+		p.commentsList.add(comment);
+	}
+	/**
+	 * @author ${Ibrahim safwat}
+	 * 
+	 * @param ideaID
+	 *            ID of the idea that the user wants to add the comment to
+	 * @param comment
+	 *            Comment to be added to list of comments of the idea
+	 */
+	public void addCommentToIdea(long ideaID, Comment comment) {
+		Idea i = Idea.findById(ideaID);
+		i.commentsList.add(comment);
+	}
 
 }
