3c218ef95408f10057bbea943bf4400b8a147537
mai.jt4@gmail.com <mai.jt4@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-06 15:29:40 +0000

    C1S6 added documentation

--- trunk/Ideas Management/app/models/Invitation.java
+++ trunk/Ideas Management/app/models/Invitation.java
@@ -13,19 +13,61 @@ import play.db.jpa.Model;
 @Entity
 public class Invitation extends Model{
 
+	
+	/**
+	 *  String email that will receive the invitation
+	 */
 	public String email;
-    public Date date_of_sending;
+	
+	/**
+	 *  Date dateOfSending of sending the invitation
+	 */
+    public Date dateOfSending;
+    
+    /**
+     *  String role that will be assigned to the user if accept the invitation
+     */
     public String role;
     
  
-    
+   /**
+    *   User sender that sent the invitation    
+    */
     @ManyToOne
     public User sender;
+    
+    /**
+     *   Organization organization that sent the invitation    
+     */ 
     @ManyToOne
     public Organization organization;
+    
+    /**
+     *   MainEntity entity that sent the invitation    
+     */ 
     @ManyToOne
     public MainEntity entity;
 	
+    
+    /**
+	 * Constructor that initializes the destination (email) of the invitation and
+	 * the entity,the organization that sends the invitation and the user
+	 * who sent the invitation
+	 * 
+	 * @author Mai Magdy
+	 * 
+	 * @param email
+	 *              String email that represents the destination
+	 *            
+	 * @param entity
+	 *             MainEntity that sents the invitation
+	 *             
+	 * @param organization
+	 *             Organization that sends the invitation
+	 *             
+	 * @param sender
+	 *              User that has sent the invitations
+	 */
    
     public Invitation(String email,MainEntity entity,Organization organization,String role,
     		User sender){
@@ -34,7 +76,7 @@ public class Invitation extends Model{
         this.entity=entity;
         this.sender = sender;
         this.role=role;
-        this.date_of_sending=new Date();
+        this.dateOfSending=new Date();
         
     }
 
