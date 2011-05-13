cfa81560973d64b2a749024978d3ab965e91a061
aliaelbolock@gmail.com <aliaelbolock@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-13 23:53:00 +0000

    C3S15 description: created the model class TopicRequest

---
+++ trunk/Ideas Management/app/models/TopicRequest.java
@@ -0,0 +1,100 @@
+package models;
+
+import play.db.jpa.Model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import play.data.validation.Required;
+
+
+@Entity
+public class TopicRequest extends Model{
+	
+		/**
+		 * The user requesting the topic
+		 */
+		@Required
+		@ManyToOne
+		public User requester;
+
+		/**
+		 * The entity that the user wants the requested topic to be posted in
+		 */
+		@Required
+		@ManyToOne
+		public MainEntity entity;
+
+	
+		/**
+		 * A title of the requested topic
+		 */
+		@Required
+		public String title;
+		
+		/**
+		 * The description of the requested topic
+		 */
+		@Required
+		public String description;
+		
+		/**
+		 * The privacy level of the requested topic
+		 */
+		@Required
+		public int privacyLevel;
+		
+		/**
+		 * The message the requester adds to his request
+		 */
+		public String message;
+		
+		
+		
+		
+		
+
+		/**
+		 * Constructor for TopicRequest
+		 * 
+		 * @author Alia El Bolock
+		 * 
+		 * @param requester
+		 * 
+		 * @param entity
+		 * 
+		 * @param title
+		 * 
+		 * @param description
+		 * 
+		 * @param privacyLevel
+		 * 
+		 * @param message
+		 */
+		public TopicRequest(User requester, MainEntity entity, 
+				String title, String description, int privacyLevel, String message) {
+			this.requester = requester;
+			this.entity = entity;
+			this.title = title;
+			this.description = description;
+			this.privacyLevel = privacyLevel;
+			this.message = message;
+		}
+		//TODO
+
+		/**
+		 * Overrides the toString method to get the title of the requested topic and the requester of the topic
+		 * 
+		 * @author Alia El Bolock
+		 * 
+		 * @return String
+		 */
+		public String toString() {
+			
+			String topicRequest = "Title: " + this.title + " Requester: " + this.requester;
+			return topicRequest;
+		}
+	}
+
+
