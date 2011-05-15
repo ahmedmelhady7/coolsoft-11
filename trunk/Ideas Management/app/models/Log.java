4667a7be7363ea2894fcc706a4ddb56b2157996c
lama.ashraf@gmail.com <lama.ashraf@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-15 17:56:07 +0000

    C1S8 modified the log model and made it extends CoolModel

--- trunk/Ideas Management/app/models/Log.java
+++ trunk/Ideas Management/app/models/Log.java
@@ -1,20 +1,99 @@
 package models;
 
-import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.lang.reflect.*;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 
-import play.db.jpa.Model;
+import models.User;
+//import models.Project;
+
+import controllers.Security;
+
+import play.db.jpa.*;
 @Entity
-public class Log extends Model{
+public class Log extends CoolModel{
 	public String actionDescription;
-	public Timestamp time;
-	
-	@ManyToOne
-	public Organization organization;
+	public long time;
 	
 	
 	
+	/**
+     * Adds a log with the models attached
+     * @param message log message
+     * @param models all models attached to this log
+     */
+	public static void addLog(String message, CoolModel... models) {
+        Log log = new Log();
+        log.actionDescription = message;
+        log.time = new Date().getTime();
+        log.save();
 
+        List<Class> clazzes = new ArrayList<Class>();
+        for (CoolModel model : models) {
+                model.logs.add(log);
+                if (clazzes.contains(model.getClass())) {
+                        model.refresh();
+                } else {
+                        clazzes.add(model.getClass());
+                }
+                model.save();
+        }
+}
+	
+	/**
+     * Adds a log with the connected user attached to the models
+     * @param message log message
+     * @param models all  models attached to this log
+     */
+    public static void addUserLog(String message, CoolModel... models) {
+            CoolModel[] newModels = Arrays.copyOf(models, models.length + 1);
+            newModels[models.length] = Security.getConnected();
+            addLog(message, newModels);
+    }
+    
+	
+    /**
+     * Returns the first user associated with this log (usually the log performer)
+     * @return User that performed this action/log
+     *//*
+    public User getUser() {
+            return get(User.class);
+    }
+*/
+    
+    /**
+     * Gets all models of class clazz that are associated with this log (usually one)
+     * @param clazz class to check for
+     * @return List of this class that have this log
+     */
+    /*public <T extends LogHelper> List<T> getAll(Class<T> clazz) {
+            try {
+                    // find the method with the following signature (remember Object... maps to an array)
+                    Method method = clazz.getMethod("find", String.class, Object[].class);
+                    
+                    // invoke this method on a null object (hence a static method), remember there's a difference between sending the class name as a ?, and hardcoding it in the query string
+                    Object result = method.invoke(null, "select m from " + clazz.getName() + " m join m.logs as l where l = ?", new Object[] {this});
+                    
+                    // cast the result, and fetch the list
+                    return ((JPASupport.JPAQuery) result).fetch();
+            } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+            }
+    }
+    
+    *//**
+     * Returns the first class of type clazz associated with this log
+     * @param clazz class to check for, example: Meeting.class, User.class, etc
+     * @return First model that mathces
+     *//*
+    public <T extends LogHelper> T get(Class<T> clazz) {
+            List<T> all = (List<T>) getAll(clazz);
+            return all.isEmpty() ? null : all.get(0);
+    }*/
 }
