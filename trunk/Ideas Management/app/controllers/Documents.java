30c9bb716c0422a7c4516c6d5f34b0882a530865
ibrahim.al.khayat@gmail.com <ibrahim.al.khayat@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-16 19:19:39 +0000

    C2S28 Added docs to user profile

--- trunk/Ideas Management/app/controllers/Documents.java
+++ trunk/Ideas Management/app/controllers/Documents.java
@@ -17,33 +17,69 @@ import controllers.CRUD.ObjectType;
 
 public class Documents extends CRUD {
 
-	public static void newDocument() {
+	/**
+	 * Renders the page for creating a new document
+	 * 
+	 * @author Ibrahim Al-Khayat
+	 * @param id
+	 *            id of the owner user/organization
+	 */
+	public static void newDocument(long id) {
 		boolean isOrganization = false;
-		Organization organization = null;
 		User user = Security.getConnected();
-		long id = user.id;
-		// if (isOrganization) {
-		// organization = Organization.findById(id);
-		// owner = organization.creator;
-		// } else {
-		// owner = User.findById(id);
-		// }
-		render(id, user, organization, isOrganization);
+		render(id, user, isOrganization);
 	}
 
+	/**
+	 * Creates a new document and store it in the database
+	 * 
+	 * @author Ibrahim Al-Kahayat
+	 * @param id
+	 *            id of the owner user/organization
+	 * @param isOrganization
+	 *            flag to indicate weather the owner is an organization or not
+	 * @param title
+	 *            document title
+	 * @param data
+	 *            document body
+	 */
 	public static void createDocument(long id, boolean isOrganization,
-			String name, String data) {
-		System.out.println("Entered Create");
-		new Document(name, data, id, isOrganization).save();
+			String title, String data) {
+		new Document(title, data, id, isOrganization).save();
 	}
 
+	/**
+	 * Renders the viewsDocument HTML
+	 * 
+	 * @author Ibrahim Al-Kahayat
+	 * @param id
+	 *            id of the document
+	 */
 	public static void viewDocument(long id) {
-		System.out.println("ENTERED viewDOC");
 		Document document = Document.findById(id);
-		render(document);
+		boolean canEdit = false;
+		long userId = Security.getConnected().id;
+		if (document.isOrganization) {
+			if (((Organization) Organization
+					.findById(document.userOrganizationId)).creator.id == userId) {
+				canEdit = true;
+			}
+		} else {
+			if (userId == document.userOrganizationId) {
+				canEdit = true;
+			}
+		}
+		render(document, canEdit);
 	}
 
-	public static void listDocument() {
+	/**
+	 * Renders the listDocument HTML
+	 * 
+	 * @author Ibrahim Al-Kahayat
+	 * @param id
+	 *            id of the Organization (-1 for rendering the list for a user)
+	 */
+	public static void listDocument(long id) {
 		User user = Security.getConnected();
 		List<Document> documents = Document.find("byUserOrganizationId",
 				user.id).fetch();
@@ -52,6 +88,24 @@ public class Documents extends CRUD {
 				documents.remove(i);
 		}
 		render(user, documents);
+	}
+
+	public static void editDocument(long id) {
+		User user = Security.getConnected();
+		Document document = Document.findById(id);
+		render(user, document);
+	}
+
+	public static void updateDocument(long id, String title, String data) {
+		Document document = Document.findById(id);
+		document.data = data;
+		document.name = title;
+		document.save();
+	}
+
+	public static void deleteDocument(long id) {
+		Document document = Document.findById(id);
+		document.delete();
 	}
 
 }
