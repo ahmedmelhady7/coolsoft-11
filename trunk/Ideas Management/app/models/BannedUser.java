b17f0143790a3021bc15f80ea566744784c3c2c9
nada.ossama.kamal@gmail.com <nada.ossama.kamal@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-17 13:50:27 +0000

    C1S7 , C1S16 added the methods of the de-Restriction to the model and changed some methods concerning the restriction as to cascade

--- trunk/Ideas Management/app/models/BannedUser.java
+++ trunk/Ideas Management/app/models/BannedUser.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import controllers.Roles;
+
 import play.db.jpa.Model;
 
 @Entity
@@ -114,20 +116,25 @@ public class BannedUser extends Model {
 	}
 
 	/**
-	 *  block a user from a certain action within an
-	 * entity
+	 * block a user from a certain action within an entity and cascade that
+	 * restriction to the rest of the topics within this entity if the action
+	 * was topic related
 	 * 
 	 * @author: Nada Ossama
 	 * 
 	 * @story :C1S7
 	 * 
-	 * @param userID : long ID of the User to be blocked
+	 * @param userID
+	 *            : long ID of the User to be blocked
 	 * 
-	 * @param organizationID : long id that this entity belongs to
+	 * @param organizationID
+	 *            : long id that this entity belongs to
 	 * 
-	 * @param action : String action that the user will be banned from
+	 * @param action
+	 *            : String action that the user will be banned from
 	 * 
-	 * @param entityID : long id the ID of that entity
+	 * @param entityID
+	 *            : long id the ID of that entity
 	 * 
 	 * @return boolean : false if found banned otherwise return true
 	 * 
@@ -151,6 +158,20 @@ public class BannedUser extends Model {
 		newBannedUser.save();
     	myBannedUser.bannedUsers.add(newBannedUser);
 		myOrganization.bannedUsers.add(newBannedUser);
+		
+		ArrayList<String> topicActions = (ArrayList<String>) Roles.getOrganizerTopicActions();
+		/**
+		 * cascade the restriction if the action is topic related
+		 */
+		if(topicActions.contains(action)){
+			MainEntity entity = MainEntity.findById(entityID);
+			List<Topic> entityTopics = entity.topicList;
+			for(int i = 0 ; i < entityTopics.size() ; i++){
+				long topicId = entityTopics.get(i).getId();
+				banFromActionInTopic(userID, organizationID, action, topicId);
+			}
+			
+		}
 		return true;
 	}
 
@@ -230,6 +251,157 @@ public class BannedUser extends Model {
 		myBannedUser.bannedUsers.add(newBannedUser);
 		myOrganization.bannedUsers.add(newBannedUser);
 		return true;
+	}
+	/**
+	 * de-restricts an organizer from a certain action within a specified topic
+	 * 
+	 * @author Nada Ossama
+	 * 
+	 * @story C1S19
+	 * 
+	 * @param userID : long user ID is the id of the organizer to be direstricted
+	 * @param action : String action to be de restricted from
+	 * @param topicID : long topicID is the id of the topic to be derestricted from
+	 */
+	
+	
+	public static void deRestrictFromTopic(long userID  ,String action , long topicID ){
+		User user = User.findById(userID);
+		Topic topic = Topic.findById(topicID);
+		MainEntity entity = topic.entity;
+		long entityID = entity.getId();
+		Organization organization = entity.organization;
+		
+		BannedUser restricted = BannedUser.find( "select bu from BannedUser bu where bu.organization = ? and bu.bannedUser = ?" +
+				" and bu.action like ? and bu.resourceType = ? and bu.resourceID = ? "  ,organization,user,action,"topic",topicID ).first();
+		
+		boolean flag = false;
+		if(restricted == null){
+			flag = true;
+		}
+		System.out.println( "null restricted" +flag+ "user:"+userID + "action" + action +"topicID:" + topicID );
+		
+		if(restricted != null){
+			user.bannedUsers.remove(restricted);
+			organization.bannedUsers.remove(restricted);
+			restricted.delete();
+		}
+		
+		
+		
+		BannedUser restrictedInEntity = BannedUser.find( "select bu from BannedUser bu where bu.organization = ? and bu.bannedUser = ?" +
+				" and bu.action like ? and bu.resourceType = ? and bu.resourceID = ? "  ,organization,user,action,"entity",entityID ).first();
+		if(restrictedInEntity != null){
+			deRestrictFromEntityNoCascading(userID, action, entityID);
+		}
+		
+		
+		
+	}
+	
+	/**
+	 * de-restricts a certain user from a certain action in a certain entity
+	 * without cascading the derestricton process to the topics within that
+	 * entity
+	 * 
+	 * @author: Nada Ossama
+	 * 
+	 * @story: C1S16
+	 * 
+	 * 
+	 * @param userID
+	 *            : long userID is the id of the user to be de-restricted
+	 * @param action
+	 *            :String action is the action to be de-restricted from
+	 * @param entityID
+	 *            : long entityID is the id of the entity to be de-restricted
+	 *            from
+	 */
+	public static void deRestrictFromEntityNoCascading(long userID , String action , long entityID){
+	
+		User user = User.findById(userID);
+		MainEntity entity = MainEntity.findById(entityID);
+		Organization organization = entity.organization;
+		
+		BannedUser restricted = BannedUser.find( "select bu from BannedUser bu where bu.organization = ? and bu.bannedUser = ?" +
+				" and bu.action like ? and bu.resourceType = ? and bu.resourceID = ? "  ,organization,user,action,"entity",entityID).first();
+		
+		boolean flag = false;
+		if(restricted == null){
+			flag = true;
+		}
+		System.out.println( "null restricted" +flag+ "user:"+userID + "action" + action +"entityID:" + entityID );
+		
+		if(restricted !=null){
+			user.bannedUsers.remove(restricted);
+			organization.bannedUsers.remove(restricted);
+			restricted.delete();
+			
+		}
+		
+		
+		System.out.println("DELETEDDDD");
+		
+	}
+
+	/**
+	 * de-restrict a user from a certain action in the whole entity and the
+	 * de-restriction process is cascaded to all the topics within that entity
+	 * 
+	 * @author Nada Ossama
+	 * 
+	 * @story C1S16
+	 * 
+	 * @param userID
+	 *            : long userID is the id of the user to be de-restricted
+	 * @param action
+	 *            : String action is the action to be de-restricted from
+	 * @param entityID
+	 *            : long entityID is the id of the entity to be de-restricted in
+	 */
+	
+	public static void deRestrictFromEntityWithCascading(long userID , String action , long entityID){
+		
+		User user = User.findById(userID);
+		MainEntity entity = MainEntity.findById(entityID);
+		Organization organization = entity.organization;
+		List<Topic> topicsList = entity.topicList;
+		List<String> topicActions = Roles.getOrganizerTopicActions();
+		
+		
+		/**
+		 * if the action passed belongs to the list of topicActions then cascade
+		 * the de-restriction
+		 */
+		if(topicActions.contains(action)){
+			for(int i = 0 ; i < topicsList.size(); i++ ){
+				Topic topic = topicsList.get(i);
+				long topicID = topic.getId();
+				deRestrictFromTopic(userID, action, topicID);
+				
+			}
+		}
+		/**
+		 * whether the action belongs to topics or not it will be de-restricted in the entity 
+		 */
+		
+		BannedUser restricted = BannedUser.find( "select bu from BannedUser bu where bu.organization = ? and bu.bannedUser = ?" +
+				" and bu.action like ? and bu.resourceType = ? and bu.resourceID = ? "  ,organization,user,action,"entity",entityID).first();
+		
+		boolean flag = false;
+		if(restricted == null){
+			flag = true;
+		}
+		System.out.println( "null restricted" +flag+ "user:"+userID + "action" + action +"entityID:" + entityID );
+		
+		if(restricted != null){
+			user.bannedUsers.remove(restricted);
+			organization.bannedUsers.remove(restricted);
+			restricted.delete();
+		}
+		
+		System.out.println("DELETEDDDD");
+		
 	}
 
 }
