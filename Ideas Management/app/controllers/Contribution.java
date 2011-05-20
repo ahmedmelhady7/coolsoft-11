e50aeccbeea4e779aac89d3011f50d78357815f5
fadzy.amir <fadzy.amir@gmail.com>
2011-05-20 20:58:05 +0000

    Fady Amir c4s11
    Creating controller class Contribution that takes the id of an organization passed through viewProfile.html and get all the users of that organization and sort them according to the contribution counter and render the sorted list to the contribution.html to be viewed there.

---
+++ Ideas Management/app/controllers/Contribution.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.util.List;
+
+import play.db.jpa.Blob;
+import play.mvc.Controller;
+import models.Document;
+import models.Organization;
+import models.Picture;
+import models.User;
+
+public class Contribution extends Controller
+{
+	/**
+	 * 
+	 * @author Fady Amir
+	 * 
+	 * @story c4s11 : contribution
+	 * @param Id
+	 * @return list
+	 * 
+	 */
+	
+	
+	public static void contribution(long id)
+	{
+		//User user = Security.getConnected();
+		
+		List<User> users = Users.getEnrolledUsers((Organization)Organization.findById(id));
+		
+		User temp;
+		int n=users.size();
+
+		for(int i=0; i<n; i++)
+		{
+		   for(int j = 1; j < (n-i); j++)
+		   {
+		        if((users.get(j-1).communityContributionCounter) > (users.get(j).communityContributionCounter))
+		        {
+		          temp = users.get(j-1);
+		          users.set(j-1,users.get(j));
+		          users.set(j,temp);
+		        }
+		   }
+		}
+		render(users);
+	}  
+	   
+}
