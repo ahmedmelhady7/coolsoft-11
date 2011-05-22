81eb7d5ce04fdeb4757673f36a1c5f6be61f5888
nohakhater@gmail.com <nohakhater@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-22 01:28:36 +0000

    C2S18: Added the documentation for my methods in the controllers.

--- trunk/Ideas Management/app/controllers/CreateRelationshipRequests.java
+++ trunk/Ideas Management/app/controllers/CreateRelationshipRequests.java
@@ -10,19 +10,53 @@ import play.mvc.With;
 @With(Secure.class)
 public class CreateRelationshipRequests extends CRUD {
 
+	/**
+	 * creates a request for creating a relationship between entities or topics.
+	 * 
+	 * @author Noha Khater
+	 * 
+	 * @Story C2S18
+	 * 
+	 * @param userId
+	 *            the id of the user making the request.
+	 * 
+	 * @param source
+	 *            the name of the source entity or topic.
+	 * 
+	 * @param destination
+	 *            the name of the destination entity or topic.
+	 * 
+	 * @param name
+	 *            the name of the relationship.
+	 * 
+	 * @param organisationId
+	 *            the id of the organisation in which the request was made.
+	 * 
+	 * @param entityId
+	 *            the id of the entity from which the request page was accessed.
+	 * 
+	 * @param topicId
+	 *            the id of the topic from which the request page was accessed.
+	 * 
+	 * @param type
+	 *            the type of the request whether it's for an entity or topic
+	 *            (0=entity, 1=topic).
+	 * 
+	 */
 	public static void createRequest(long userId, String source,
 			String destination, String name, long organisationId,
 			long entityId, long topicId, int type) {
 		User user = User.findById(userId);
 		Organization organisation = Organization.findById(organisationId);
 		if (type == 0) {
-			if (Organizations.isDuplicateRequest(source, destination, name, organisationId, type, 1,
-					0, 0)) {
-				System.out.println(organisation.createRelationshipRequest.size());
+			if (Organizations.isDuplicateRequest(source, destination, name,
+					organisationId, type, 1, 0, 0)) {
+				System.out.println(organisation.createRelationshipRequest
+						.size());
 				redirect("MainEntitys.viewEntity", entityId, "Request created");
 				return;
 			}
-				
+
 			MainEntity sourceEntity = MainEntity.find("byNameAndOrganization",
 					source, organisation).first();
 			MainEntity destinationEntity = MainEntity.find(
@@ -34,9 +68,10 @@ public class CreateRelationshipRequests extends CRUD {
 			System.out.println(organisation.createRelationshipRequest.size());
 			redirect("MainEntitys.viewEntity", entityId, "Request created");
 		} else {
-			if (Organizations.isDuplicateRequest(source, destination, name, organisationId, type, 1,
-					0, 0)) {
-				System.out.println(organisation.createRelationshipRequest.size());
+			if (Organizations.isDuplicateRequest(source, destination, name,
+					organisationId, type, 1, 0, 0)) {
+				System.out.println(organisation.createRelationshipRequest
+						.size());
 				redirect("MainEntitys.viewEntity", entityId, "Request created");
 				return;
 			}
--- trunk/Ideas Management/app/controllers/RenameEndRelationshipRequests.java
+++ trunk/Ideas Management/app/controllers/RenameEndRelationshipRequests.java
@@ -11,63 +11,135 @@ import play.mvc.With;
 
 @With(Secure.class)
 public class RenameEndRelationshipRequests extends CRUD {
-	
+
+	/**
+	 * creates a request for renaming an entity or topic relationship
+	 * 
+	 * @author Noha Khater
+	 * 
+	 * @Story C2S18
+	 * 
+	 * @param userId
+	 *            the id of the user making the request.
+	 * 
+	 * @param organisationId
+	 *            the id of the organisation to which the entity or topic
+	 *            belongs.
+	 * 
+	 * @param entityId
+	 *            the id of the entity from which the request page was accessed.
+	 * 
+	 * @param topicId
+	 *            the id of the topic from which the request page was accessed.
+	 * 
+	 * @param relationId
+	 *            the id of the relationship to be renamed.
+	 * 
+	 * @param type
+	 *            the type of the relationship request whether for an entity or
+	 *            topic. (0=entity, 1=topic).
+	 * 
+	 * @param requestType
+	 *            the type of the request whether rename or delete. (0=delete,
+	 *            1=rename).
+	 * 
+	 * @param newName
+	 *            the new name for the relationship to be renamed.
+	 */
 	public static void renameRequest(long userId, long organisationId,
-			long entityId, long topicId,
-			long relationId, int type, int requestType, String newName) {
+			long entityId, long topicId, long relationId, int type,
+			int requestType, String newName) {
 		User user = User.findById(userId);
 		Organization organisation = Organization.findById(organisationId);
 		if (type == 0) {
-//			if (Organizations.isDuplicateRequest(null, null, null, organisationId, type, 0,
-//					requestType, relationId)) {
-//				System.out.println(organisation.renameEndRelationshipRequest.size());
-//				redirect("MainEntitys.viewEntity", entityId, "Request created");
-//				return;
-//			}
-			EntityRelationship relation = EntityRelationship.findById(relationId);
-			RenameEndRelationshipRequest renameRequest = new RenameEndRelationshipRequest(user, organisation,
-					relation, type, requestType, newName);
+			// if (Organizations.isDuplicateRequest(null, null, null,
+			// organisationId, type, 0,
+			// requestType, relationId)) {
+			// System.out.println(organisation.renameEndRelationshipRequest.size());
+			// redirect("MainEntitys.viewEntity", entityId, "Request created");
+			// return;
+			// }
+			EntityRelationship relation = EntityRelationship
+					.findById(relationId);
+			RenameEndRelationshipRequest renameRequest = new RenameEndRelationshipRequest(
+					user, organisation, relation, type, requestType, newName);
 			renameRequest.save();
-			System.out.println(organisation.renameEndRelationshipRequest.size());
+			System.out
+					.println(organisation.renameEndRelationshipRequest.size());
 			redirect("MainEntitys.viewEntity", entityId, "Request created");
 		} else {
-//			if (Organizations.isDuplicateRequest(null, null, null, organisationId, type, 0,
-//					requestType, relationId)) {
-//				System.out.println(organisation.renameEndRelationshipRequest.size());
-//				redirect("Topics.show", topicId, "Request created");
-//				return;
-//			}
+			// if (Organizations.isDuplicateRequest(null, null, null,
+			// organisationId, type, 0,
+			// requestType, relationId)) {
+			// System.out.println(organisation.renameEndRelationshipRequest.size());
+			// redirect("Topics.show", topicId, "Request created");
+			// return;
+			// }
 			TopicRelationship relation = TopicRelationship.findById(relationId);
 			RenameEndRelationshipRequest renameRequest = new RenameEndRelationshipRequest(
-					user, organisation,relation, type, requestType,newName);
+					user, organisation, relation, type, requestType, newName);
 			renameRequest.save();
-			System.out.println(organisation.renameEndRelationshipRequest.size());
-			redirect("Topics.show", topicId, "Request created");
-		}		
-	}
-	
-	public static void deleteRequest(long userId, long organisationId,
-			long entityId, long topicId,
-			long relationId, int type, int requestType) {
-		User user = User.findById(userId);
-		Organization organisation = Organization.findById(organisationId);
-		if (type == 0) {
-			EntityRelationship relation = EntityRelationship.findById(relationId);
-			RenameEndRelationshipRequest renameRequest = new RenameEndRelationshipRequest(user, organisation,
-					relation, type, requestType, null);
-			renameRequest.save();
-			System.out.println(organisation.renameEndRelationshipRequest.size());
-			redirect("MainEntitys.viewEntity", entityId, "Request created");
-		} else {
-			TopicRelationship relation = TopicRelationship.findById(relationId);
-			RenameEndRelationshipRequest renameRequest = new RenameEndRelationshipRequest(
-					user, organisation,relation, type, requestType,null);
-			renameRequest.save();
-			System.out.println(organisation.renameEndRelationshipRequest.size());
+			System.out
+					.println(organisation.renameEndRelationshipRequest.size());
 			redirect("Topics.show", topicId, "Request created");
 		}
 	}
-	
-	
+
+	/**
+	 * creates a request for deleting an entity or topic relationship
+	 * 
+	 * @author Noha Khater
+	 * 
+	 * @Story C2S18
+	 * 
+	 * @param userId
+	 *            the id of the user making the request.
+	 * 
+	 * @param organisationId
+	 *            the id of the organisation to which the entity or topic
+	 *            belongs.
+	 * 
+	 * @param entityId
+	 *            the id of the entity from which the request page was accessed.
+	 * 
+	 * @param topicId
+	 *            the id of the topic from which the request page was accessed.
+	 * 
+	 * @param relationId
+	 *            the id of the relationship to be deleted.
+	 * 
+	 * @param type
+	 *            the type of the relationship request whether for an entity or
+	 *            topic. (0=entity, 1=topic).
+	 * 
+	 * @param requestType
+	 *            the type of the request whether rename or delete. (0=delete,
+	 *            1=rename).
+	 *
+	 */
+	public static void deleteRequest(long userId, long organisationId,
+			long entityId, long topicId, long relationId, int type,
+			int requestType) {
+		User user = User.findById(userId);
+		Organization organisation = Organization.findById(organisationId);
+		if (type == 0) {
+			EntityRelationship relation = EntityRelationship
+					.findById(relationId);
+			RenameEndRelationshipRequest renameRequest = new RenameEndRelationshipRequest(
+					user, organisation, relation, type, requestType, null);
+			renameRequest.save();
+			System.out
+					.println(organisation.renameEndRelationshipRequest.size());
+			redirect("MainEntitys.viewEntity", entityId, "Request created");
+		} else {
+			TopicRelationship relation = TopicRelationship.findById(relationId);
+			RenameEndRelationshipRequest renameRequest = new RenameEndRelationshipRequest(
+					user, organisation, relation, type, requestType, null);
+			renameRequest.save();
+			System.out
+					.println(organisation.renameEndRelationshipRequest.size());
+			redirect("Topics.show", topicId, "Request created");
+		}
+	}
 
 }
