2fc3ed8a0b25520ad11235122ca2e404c8fe76fb
m.hisham.sa <m.hisham.sa@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-22 02:24:16 +0000

    added the check for duplicate relationships between Entities/Topics/Tags

--- trunk/Ideas Management/app/controllers/TopicRelationships.java
+++ trunk/Ideas Management/app/controllers/TopicRelationships.java
@@ -9,12 +9,11 @@ import javax.xml.transform.Source;
 import play.db.jpa.Model;
 import models.*;
 
-
 /**
- * @author Mohamed
+ * @author Mohamed Hisham
  * 
  * @story C2S5
- *
+ * 
  */
 public class TopicRelationships extends CRUD {
 
@@ -33,40 +32,75 @@ public class TopicRelationships extends CRUD {
 	 * 
 	 * @param destinationId
 	 *            : id of the second Topic to be related
+	 *            
+	 * @return boolean
 	 */
-	public static void createRelationship(String name, long sourceId,
+	public static boolean createRelationship(String name, long sourceId,
 			long destinationId) {
 		Topic source = Topic.findById(sourceId);
 		Topic destination = Topic.findById(destinationId);
 
 		TopicRelationship relation = new TopicRelationship(name, source,
 				destination);
-		
-		relation.source.relationsSource.add(relation);
-		relation.destination.relationsDestination.add(relation);
-		if(!isDuplicate(name, relation.source.entity.organization.relationNames))
-			relation.source.entity.organization.relationNames.add(name);
-		
-		relation.save();
-		Organization organization = relation.source.entity.organization;
-		organization.save();
-		
-		for (int i = 0; i < Users.getEntityOrganizers(source.entity).size(); i++) {
-			Notifications.sendNotification(Users.getEntityOrganizers(source.entity)
-					.get(i).id, source.entity.organization.id, "Organization",
-					"a new relation \"" + name
-							+ "\" is created now between topics \""
-							+ source.title + "\" and \"" + destination.title
-							+ "\".");
+		if (!relationDuplicate(relation)) {
+			relation.source.relationsSource.add(relation);
+			relation.destination.relationsDestination.add(relation);
+			if (!isDuplicate(name,
+					relation.source.entity.organization.relationNames))
+				relation.source.entity.organization.relationNames.add(name);
+
+			relation.save();
+			Organization organization = relation.source.entity.organization;
+			organization.save();
+
+			if (!relation.source.entity.equals(relation.destination.entity)) {
+				for (int i = 0; i < Users.getEntityOrganizers(source.entity)
+						.size(); i++) {
+					Notifications.sendNotification(Users.getEntityOrganizers(
+							source.entity).get(i).id,
+							source.entity.organization.id, "Organization",
+							"a new relation \"" + name
+									+ "\" is created now between topics \""
+									+ source.title + "\" and \""
+									+ destination.title + "\".");
+				}
+			}
+			for (int i = 0; i < Users.getEntityOrganizers(destination.entity)
+					.size(); i++) {
+				Notifications.sendNotification(Users.getEntityOrganizers(
+						destination.entity).get(i).id,
+						source.entity.organization.id, "Organization",
+						"a new relation \"" + name
+								+ "\" is created now between topics \""
+								+ source.title + "\" and \""
+								+ destination.title + "\".");
+			}
+			return true;
 		}
-		for (int i = 0; i < Users.getEntityOrganizers(destination.entity).size(); i++) {
-			Notifications.sendNotification(Users.getEntityOrganizers(destination.entity)
-					.get(i).id, source.entity.organization.id, "Organization",
-					"a new relation \"" + name
-							+ "\" is created now between topics \""
-							+ source.title + "\" and \"" + destination.title
-							+ "\".");
+		return false;
+	}
+	
+	/**
+	 * checks if a relation is duplicate(returns true) or not(returns false)
+	 * 
+	 * @author Mohamed Hisham
+	 * 
+	 * @param relation : the relation being checked for duplicate
+	 * 
+	 * @return boolean
+	 */
+	public static boolean relationDuplicate(TopicRelationship relation) {
+		for (int i = 0; i < relation.source.relationsSource.size(); i++) {
+			if (relation.source.relationsSource.get(i).source
+					.equals(relation.source)
+					&& relation.source.relationsSource.get(i).destination
+							.equals(relation.destination)
+					&& relation.source.relationsSource.get(i).name
+							.equals(relation.name)) {
+				return true;
+			}
 		}
+		return false;
 	}
 
 	/**
@@ -136,16 +170,19 @@ public class TopicRelationships extends CRUD {
 	 * 
 	 * @author Mohamed Hisham
 	 * 
-	 * @param relationName : name of the relation
+	 * @param relationName
+	 *            : name of the relation
 	 * 
-	 * @param relationNames : list of relation names in the organization
+	 * @param relationNames
+	 *            : list of relation names in the organization
 	 * 
 	 * @return boolean
 	 */
-	public static boolean isDuplicate(String relationName, ArrayList<String> relationNames){
-		for(int i = 0; i < relationNames.size(); i++){
-			if(relationName.equals(relationNames.get(i)))
-				return true;	
+	public static boolean isDuplicate(String relationName,
+			ArrayList<String> relationNames) {
+		for (int i = 0; i < relationNames.size(); i++) {
+			if (relationName.equals(relationNames.get(i)))
+				return true;
 		}
 		return false;
 	}
