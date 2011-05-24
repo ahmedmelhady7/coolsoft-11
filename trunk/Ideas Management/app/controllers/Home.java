62d086dd6970f0bfeea2277d725f55c0be4611cf
mostafa.aly0@gmail.com <mostafa.aly0@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-24 20:44:34 +0000

    C1S10 : removed the methods that were moved to the controller accounts

--- trunk/Ideas Management/app/controllers/Home.java
+++ trunk/Ideas Management/app/controllers/Home.java
@@ -2,6 +2,9 @@ package controllers;
 
 import java.util.Date;
 
+import notifiers.Mail;
+
+import models.Invitation;
 import models.User;
 import play.mvc.Controller;
 
@@ -55,64 +58,5 @@ public class Home extends Controller {
 		render();
 	}
 	
-	/**
-	 * renders the page responsible for the registration
-	 * 
-	 * @story C1S10
-	 * 
-	 * @author Mostafa Ali
-	 * 
-	 */
-	public static void register() {
-		render();
-	}
 	
-	/**
-	 * 
-	 * This method is responsible for adding an unregistered user
-	 * 
-	 * @author Mostafa Ali
-	 * 
-	 * @story C1S9
-	 * 
-	 * @param email
-	 * 				: the user's email
-	 * 
-	 * @param firstName
-	 * 				: the user's first name
-	 * 
-	 * @param lastName
-	 * 				: the user's last name
-	 * 
-	 * @param username
-	 *              : the user's username
-	 * 
-	 * @param password
-	 * 				: the user's password
-	 * 
-	 * @param profession
-	 * 				: the user's profession
-	 * 
-	 * @param communityContributionCounter
-	 * 	            : the user's community contribution counter
-	 * 
-	 * @param dateofBirth
-	 * 				: the user's date of birth
-	 * 
-	 * @param country
-	 * 				: the user's country
-	 * 
-	 */
-	
-	public static void addUser(String email, String password, String firstName,
-			String lastName, String username, int communityContributionCounter,
-			Date dateofBirth, String country, String profession)
-	{
-			User u = new User(email,username, password, firstName, lastName," "," ",  communityContributionCounter, dateofBirth, country, profession);
-			u._save();
-		
-		//render(email,username,password);
-	}
-
-
 }
