45aceddf54c717eb7f049def4e42f862e89bcd10
monica.yousry90@gmail.com <monica.yousry90@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-25 21:39:35 +0000

    my junit test (i have only one since i don't alter any data except for a counter :)) -the filter (all in html page) -the sort i just sort a list taken from another developer (loaay)

---
+++ trunk/Ideas Management/test/c4/monica.java
@@ -0,0 +1,49 @@
+package c4;
+
+import java.util.List;
+
+import play.test.*;
+import models.Idea;
+import models.MainEntity;
+import models.Organization;
+import models.Topic;
+
+import org.junit.*;
+
+public class monica extends UnitTest {
+	@Test
+	public void checkIncrementView() {
+		List<Topic> topics = Topic.findAll();
+		Topic topic = topics.get(0);
+		int viewbefore = topic.viewed;
+		topic.incrmentViewed();
+		topic.save();
+		int viewafter = topic.viewed;
+		assertEquals(viewafter, viewbefore + 1);
+
+		List<Idea> ideas = Idea.findAll();
+		Idea idea = ideas.get(0);
+		viewbefore = idea.viewed;
+		idea.incrmentViewed();
+		idea.save();
+		viewafter = idea.viewed;
+		assertEquals(viewafter, viewbefore + 1);
+
+		List<Organization> orgs = Organization.findAll();
+		Organization org = orgs.get(0);
+		viewbefore = org.viewed;
+		org.incrmentViewed();
+		org.save();
+		viewafter = org.viewed;
+		assertEquals(viewafter, viewbefore + 1);
+
+		List<MainEntity> entities = MainEntity.findAll();
+		MainEntity entity = entities.get(0);
+		viewbefore = entity.viewed;
+		entity.incrmentViewed();
+		entity.save();
+		viewafter = entity.viewed;
+		assertEquals(viewafter, viewbefore + 1);
+	}
+
+}
