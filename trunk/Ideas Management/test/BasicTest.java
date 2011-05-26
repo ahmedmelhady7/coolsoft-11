f4037e1c9508f00d87da295bec0d1f31352b3330
nohakhater@gmail.com <nohakhater@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-05-26 15:24:51 +0000

    Committed the rest of the unit tests.

---
+++ trunk/Ideas Management/test/BasicTest.java
@@ -0,0 +1,138 @@
+
+import org.junit.*;
+
+import controllers.Roles;
+import controllers.Topics;
+import controllers.UserRoleInOrganizations;
+
+import java.util.*;
+
+import play.test.*;
+import models.*;
+
+public class BasicTest extends UnitTest {
+
+	@Before
+	public void setup() {
+		Fixtures.deleteAll();
+	}
+
+	@Test
+	public void canViewTopic() {
+		// Create a new topic and all its necessary attributes
+
+		Role.createIdeaDeveloperRole();
+		Role.createOrganizerRole();
+		Role.createAdminRole();
+		Role.createOrganizationLeadRole();
+
+		User admin = new User("admin@coolsoft.com", "admin", "1234", "Mr.",
+				"admin", "What is our company's name?", "coolsoft", 0,
+				"11/11/1990", "Egypt", "Prgrammer");
+		admin.isAdmin = true;
+		admin._save();
+
+		User mai = new User("mai.jt4@gmail.com", "mai", "1234", "Mai", "Magdy",
+				"What is our company's name?", "coolsoft", 0, "9/14/1990", "Egypt", "student");
+		mai._save();
+
+		User sharaf = new User("sharaf@eg.gov", "sharaf", "1234", "Asam",
+				"Sharaf", "What is our company's name?", "coolsoft", 0,
+				"2/14/1955", "Egypt", "Primenister").save();
+
+		Organization egypt = new Organization("Egypt", admin, 2, false,
+				"The National Egyptian Organization").save();
+
+		Organization gov = new Organization("الحكومة المصرية", sharaf, 0, true,
+				"").save();
+
+		MainEntity elAhly = new MainEntity("El-Ahly", "Al-Ahly Club Lovers",
+				egypt, true).save();
+
+		MainEntity govHead = new MainEntity("الوزارة", "راسه مجلس الوزراء",
+				gov, true).save();
+
+		Role ideadeveloper = Roles.getRoleByName("idea developer");
+		Role OrganizationLead = Roles.getRoleByName("organizationLead");
+
+		Topic matchChaos = new Topic(
+				"Chaos in egyptian football matches",
+				"try to avoid chaos and fights during matches due to different points of view",
+				2, admin, elAhly, true).save();
+		elAhly.topicList.add(matchChaos);
+		elAhly._save();
+
+		Topic govImp = new Topic("التعديلات الدستورية", "اقتراحات", 1, sharaf,
+				govHead, false).save();
+		govHead.topicList.add(govImp);
+		govHead._save();
+
+		
+		assertNotNull(matchChaos);
+		assertEquals("Chaos in egyptian football matches", matchChaos.title);
+		assertEquals(1, elAhly.topicList.size());
+		
+		assertEquals(true, matchChaos.canView(admin));
+
+		assertEquals(false, matchChaos.canView(mai));
+
+		assertEquals(true, govImp.canView(sharaf));
+		UserRoleInOrganizations.addEnrolledUser(sharaf, gov, OrganizationLead);
+		assertEquals(true, govImp.canView(sharaf));
+
+		UserRoleInOrganizations.addEnrolledUser(mai, egypt, ideadeveloper,
+				matchChaos.id, "topic");
+
+		assertEquals(true, matchChaos.canView(mai));
+
+		assertEquals(true, govImp.isDeletable());
+
+		Idea i7 = new Idea("Blank", "Blank", sharaf, govImp);
+		i7.save();
+		sharaf.save();
+		govImp.ideas.add(i7);
+		govImp.save();
+
+		assertEquals(false, govImp.isDeletable());
+
+	}
+	
+	@Test
+	public void isDeletableTopic() {
+		// Create a new topic and all its necessary attributes
+
+		Role.createOrganizationLeadRole();
+
+		User sharaf = new User("sharaf@eg.gov", "sharaf", "1234", "Asam",
+				"Sharaf", "What is our company's name?", "coolsoft", 0,
+				"2/14/1995", "Egypt", "Primenister").save();
+
+		Organization gov = new Organization("الحكومة المصرية", sharaf, 0, true,
+				"").save();
+
+		MainEntity govHead = new MainEntity("الوزارة", "راسه مجلس الوزراء",
+				gov, true).save();
+
+		Role OrganizationLead = Roles.getRoleByName("organizationLead");
+		
+		UserRoleInOrganizations.addEnrolledUser(sharaf, gov, OrganizationLead);
+
+		Topic govImp = new Topic("التعديلات الدستورية", "اقتراحات", 1, sharaf,
+				govHead, false).save();
+		govHead.topicList.add(govImp);
+		govHead._save();
+
+		assertEquals(true, govImp.isDeletable());
+
+		Idea i7 = new Idea("Blank", "Blank", sharaf, govImp);
+		i7.save();
+		sharaf.save();
+		govImp.ideas.add(i7);
+		govImp.save();
+
+		assertEquals(false, govImp.isDeletable());
+
+	}
+
+
+}
---
+++ trunk/Ideas Management/test/OmarFaruki.java
@@ -0,0 +1,61 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import play.test.*;
+import models.Organization;
+import models.Role;
+import models.User;
+
+import org.junit.*;
+
+import controllers.Roles;
+import controllers.UserRoleInOrganizations;
+
+public class OmarFaruki extends UnitTest{
+
+	@Before
+	public void setUp() {
+	    Fixtures.deleteAll();
+	}
+	
+	@Test
+	public void getAllOrganizations() {
+		User ashraf = new User("Ashraf@guc.edu.eg", "ElKbeer", "1234",
+				"Ashraf", "Mansoor", "What is our company's name?", "coolsoft",
+				0, "2/14/1995", "Egypt",
+				"student").save();
+		User faruki = new User("Faruki@guc.edu.eg", "Faruki", "1234",
+				"Omar", "Faruki", "Favourite course?", "Faruki",
+				0, "2/14/1995", "Egypt",
+				"professor").save();
+		Role.createIdeaDeveloperRole();
+		Role.createOrganizationLeadRole();
+		Organization guc = new Organization("GUC", ashraf, 1, true,
+		"The German University in Cairo").save();
+		
+		Organization auc = new Organization("AUC", faruki, 1, true,
+		"The American University in Cairo").save();
+		
+		Role role1 = Roles.getRoleByName("organizationLead");
+		UserRoleInOrganizations.addEnrolledUser(ashraf, auc,
+				role1);
+		
+		Role role = Roles.getRoleByName("idea developer");
+		UserRoleInOrganizations.addEnrolledUser(ashraf, guc,
+				role);
+		
+		UserRoleInOrganizations.addEnrolledUser(faruki, auc,
+				role);
+		List<Organization> organizations = new ArrayList<Organization>();
+		
+		organizations.add(auc);
+		organizations.add(guc);
+		assertEquals(organizations, ashraf.getOrganizations());
+		
+		List<Organization> organizations1 = new ArrayList<Organization>();
+		organizations1.add(auc);
+		assertEquals(organizations1, faruki.getOrganizations());
+	}
+}
