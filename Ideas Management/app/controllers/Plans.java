c73b64e513cb61ffb0e3c723b4ee944d89b26910
yassmeen.hussein <yassmeen.hussein@gmail.com>
2011-05-27 14:36:10 +0000

    C5S15 modified the documentation

--- Ideas Management/app/controllers/Plans.java
+++ Ideas Management/app/controllers/Plans.java
@@ -1670,7 +1670,8 @@ public class Plans extends CoolCRUD {
 	 * This method first checks if the user is allowed to edit in the plan so he
 	 * can tag the item, searches for the tag in the global list of tags, if
 	 * found it checks if the item had the same tag already or add the new one
-	 * to the list if not it creates a new tag, save it and add it to the list
+	 * to the list if not it creates a new tag, save it and add it to the list,
+	 * it updates the log then it renders JSONObject containing the Tag.
 	 * 
 	 * @author Yasmine Elsayed
 	 * 
