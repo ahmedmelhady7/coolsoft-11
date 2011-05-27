bd69dee6cd1334cd3ccb589e21b354bd4f889343
nada.ossama.kamal <nada.ossama.kamal@gmail.com>
2011-05-27 11:47:36 +0000

    C1s7 changed the filter of topic actions

--- Ideas Management/app/controllers/Roles.java
+++ Ideas Management/app/controllers/Roles.java
@@ -6,10 +6,11 @@ import java.util.List;
 import play.*;
 import play.mvc.With;
 import models.*;
+
 /**
  * 
  * @author Nada Ossama
- *
+ * 
  */
 @With(Secure.class)
 public class Roles extends CoolCRUD {
@@ -21,11 +22,10 @@ public class Roles extends CoolCRUD {
 	 * 
 	 * @story C1S7
 	 * 
-	 * @param role 
-	 *             String the name of the role
+	 * @param role
+	 *            String the name of the role
 	 * 
-	 * @return Role 
-	 *              the Role that it's name was passed
+	 * @return Role the Role that it's name was passed
 	 */
 
 	public static Role getRoleByName(String role) {
@@ -43,27 +43,25 @@ public class Roles extends CoolCRUD {
 	 * 
 	 * @param roleName
 	 *            String name of the role
-	 * @return list<String> 
-	 *             list actions of that role
+	 * @return list<String> list actions of that role
 	 */
 
 	public static List<String> getRoleActions(String roleName) {
-////changeeeeeeeeeeeeeee
-		//if(roleName.equals("organizer")){
-		
-			Role role = Role.find(
-					"select r from Role r where r.roleName = ? ",roleName).first();
-		//}
-		//if(roleName.equals("organization lead")){
-			
-		//}
-		//if(roleName.equals("admin")){
-			
-			
-		//}
-		//if(roleName.equals("idea"))
-		
-		String actionsString  = role.actions;
+		// //changeeeeeeeeeeeeeee
+		// if(roleName.equals("organizer")){
+
+		Role role = Role.find("select r from Role r where r.roleName = ? ",
+				roleName).first();
+		// }
+		// if(roleName.equals("organization lead")){
+
+		// }
+		// if(roleName.equals("admin")){
+
+		// }
+		// if(roleName.equals("idea"))
+
+		String actionsString = role.actions;
 		String[] actionsArray = actionsString.split(";");
 		List<String> actionsList = new ArrayList();
 		for (int i = 0; i < actionsArray.length; i++) {
@@ -72,22 +70,21 @@ public class Roles extends CoolCRUD {
 		return actionsList;
 	}
 
-	
 	/**
-	 * returns the list of Organizer actions that are related to
-	 * Topics not the whole entity
+	 * returns the list of Organizer actions that are related to Topics not the
+	 * whole entity
 	 * 
 	 * @author nada ossama
-	 *  
+	 * 
 	 * @story C1S7
-	 *  
+	 * 
 	 * @return List<String> which is the list of actions
 	 */
 	public static List<String> getOrganizerTopicActions() {
 
 		List<String> allActions = getRoleActions("organizer");
 		List<String> topicActions = new ArrayList<String>();
-		for (int i = 11; i < allActions.size(); i++) {
+		for (int i = 12; i < allActions.size(); i++) {
 			topicActions.add(allActions.get(i));
 		}
 		return topicActions;
