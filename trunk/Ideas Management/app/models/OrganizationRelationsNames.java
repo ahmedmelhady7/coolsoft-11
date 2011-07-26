b5f1f6e15359c1014d5726d8ea681e1c49dcd71c
fadwasakr@gmail.com <fadwasakr@gmail.com@24ac869e-b7ca-51ec-d210-f090c9ce3346>
2011-07-26 16:58:23 +0000

    @entity

--- trunk/Ideas Management/app/models/OrganizationRelationsNames.java
+++ trunk/Ideas Management/app/models/OrganizationRelationsNames.java
@@ -1,7 +1,9 @@
 package models;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class OrganizationRelationsNames extends CoolModel {
 	
 	public String name ;
